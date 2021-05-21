89121249a067d220cc81cd55565126a77931fdf1
jnpefo <jnpefo@gmail.com>
2021-05-21 20:26:57 -0300

    parando no 4

--- rotas/rotaLogin.js
+++ rotas/rotaLogin.js
@@ -4,6 +4,7 @@ const { genrateToken, validaEmail } = require('./servicosReq3');
 const login = express.Router();
 const HTTP_OK_STATUS = 200;
 const VAZIO = 400;
+const SEIS = 6;
 
 login.post('/', (req, res) => {
   const { email, password } = req.body;
@@ -13,7 +14,7 @@ login.post('/', (req, res) => {
     return res.status(VAZIO).send({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) return res.status(VAZIO).send({ message: 'O campo "password" é obrigatório' });
-  if (String(password).length < 6) {
+  if (String(password).length < SEIS) {
     return res.status(VAZIO).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
--- rotas/rotaTalker.js
+++ rotas/rotaTalker.js
@@ -6,6 +6,10 @@ const rotaTalker = express.Router();
 
 const HTTP_OK_STATUS = 200;
 const NAO_ENCONTRADO = 404;
+const NOME_ERRADO = 400;
+const TOKEN_ERRADO = 401;
+const DEZESSEIS = 16;
+const TRES = 3;
 
 rotaTalker.get('/', rescue(async (_req, res) => {
   const talker = await middlewares.lerJson();
@@ -22,6 +26,21 @@ rotaTalker.get('/:id', rescue(async (req, res) => {
     .send({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(HTTP_OK_STATUS).json(result);
+}));
+
+rotaTalker.post('/', rescue(async (req, res, _next) => {
+  const { authentication } = req.headers;
+  const { name } = req.body;
+
+  if (!authentication) return res.status(TOKEN_ERRADO).send({ message: 'Token não encontrado' });
+  if (authentication.length !== DEZESSEIS) {
+    return res.status(TOKEN_ERRADO)
+      .send({ message: 'Token inválido' });
+  } 
+  if (!name) return res.status(NOME_ERRADO).send({ message: 'O campo "name" é obrigatório' });
+  if (name.length < TRES) {
+    return res.status(NOME_ERRADO).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
 }));
 
 module.exports = rotaTalker;
