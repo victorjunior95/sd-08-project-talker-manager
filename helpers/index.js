db4a0b3293133a9eaaf9b47bbce914902857b97f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-21 19:34:47 -0300

    cria funçao que verifica data para requisito 04

--- helpers/index.js
+++ helpers/index.js
@@ -32,7 +32,13 @@ const checkAge = (age, response) => {
 };
 
 const checkTalWatchedAt = (watchedAt, response) => {
-// TODO
+  const dateRegex = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  const isValidDate = dateRegex.test(watchedAt);
+  if (!isValidDate) {
+    return response.status(400).json({
+    message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
 };
 
 const checkTalkRate = (rate, response) => {
--- middlewares/addNewTalkerMiddleware.js
+++ middlewares/addNewTalkerMiddleware.js
@@ -4,17 +4,17 @@ const {
   checkAge,
   checkTalk,
   checkTalWatchedAt,
-  checkTalkRate, } = require('../helpers');
+  checkTalkRate } = require('../helpers');
 
 module.exports = (request, response, next) => {
   const token = request.headers.authorization;
-  const { name, talk } = request.body;
+  const { name, age, talk } = request.body;
   const { watchedAt, rate } = talk;
-  checkToken();
-  checkName();
-  checkAge();
-  checkTalk();
-  checkTalWatchedAt();
-  checkTalkRate();
+  checkToken(token, response);
+  checkName(name, response);
+  checkAge(age, response);
+  checkTalk(talk, response);
+  checkTalWatchedAt(watchedAt, response);
+  checkTalkRate(rate, response);
   next();
 };
