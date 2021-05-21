bf601296570650a982f9431c719c2c778c005e7a
JRGCast <joao.ralph1@gmail.com>
2021-05-21 13:55:44 -0300

    adicionado require path e index.html para brincar com respostas mais personalizadas.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +10,7 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => {
