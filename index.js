b50ee8f48af94d275c0faf45c7c2a575db4bd5b7
Daniel <danieldsrd@gmail.com>
2021-05-21 16:01:31 -0300

    Desafio 2

--- index.js
+++ index.js
@@ -14,15 +14,27 @@ const PORT = '3000';
 // } catch (e) {
 //   console.error(e);
 // }
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => {
-  const dataJSON = await fs.readFileSync('./talker.json', 'utf8');
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const dataJSON = await fs.readFileSync('./talker.json');
   const data = await JSON.parse(dataJSON);
-  console.log(data);
+  const personById = data.find((person) => person.id === Number(id));
+  console.log(personById);
+  if (personById) {
+    res.status(200).json(personById);
+  }
+  res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+});
+
+app.get('/talker', async (_req, res) => {
+  const dataJSON = await fs.readFileSync('./talker.json');
+  const data = await JSON.parse(dataJSON);
   res.status(200).json(data);  
 });
 
