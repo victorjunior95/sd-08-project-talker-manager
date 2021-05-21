2e6f42a2766cfe0408009bf930acdf5efc3c4d99
Bruno Wedley <brunowedley27@gmail.com>
2021-05-21 15:23:14 -0300

    adiciona requisito 2

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ app.get('/talker', (_req, res) => {
   res.status(200).json(data);
 });
 
+app.get('/talker/:id', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(showTalkers, 'utf-8'));
+  const talkerById = data.find((talker) => parseInt(talker.id, 0) === parseInt(req.params.id, 0));
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talkerById);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
