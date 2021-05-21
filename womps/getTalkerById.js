432a89c16ac74b4893517a226ffbdc2fe527d368
Bruno Martins <bruno.martins217@gmail.com>
2021-05-21 18:04:55 -0300

    fix2: Desafio 2

--- womps/getTalkerById.js
+++ womps/getTalkerById.js
@@ -5,5 +5,10 @@ module.exports = (req, res) => {
   const data = JSON.parse(fs.readFileSync(meuArquivo, 'utf-8'));
   const { id } = req.params;
   const talker = data.filter((element) => element.id === Number(id));
-  return res.status(200).send(talker[0]);
+  if (talker.length) {
+    return res.status(200).send(talker[0]);
+  }
+  return res.status(404).send({
+    message: 'Pessoa palestrante não encontrada',
+  });
 };
