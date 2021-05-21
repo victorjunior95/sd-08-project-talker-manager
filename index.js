e19da363c0826c5ff9d606705cd4cc306c8b068c
Tandy <tandyfw@gmail.com>
2021-05-21 15:41:40 -0300

    Requisito 1 concluido

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile('./talker.json', (_err, data) => {
+    res.status(HTTP_OK_STATUS).send(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {
