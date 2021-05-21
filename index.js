2d5902b1e76fe7c78ed05576e4009d7f2004933f
Tandy <tandyfw@gmail.com>
2021-05-21 16:42:00 -0300

    Requisito 3 concluido

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// reference: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+function validateEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+// Reference: https://www.ti-enxame.com/pt/javascript/gere-stringcaracteres-aleatorios-em-javascript/967048592/
+function getToken() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let index = 0; index < 16; index += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -25,6 +41,20 @@ app.get('/talker/:id', (req, res) => {
   const person = talker.find((pers) => pers.id === parseInt(id, 10));
   if (!person) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
   res.status(HTTP_OK_STATUS).send(person);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (!validateEmail(email)) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (password.toString().length < 6) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const token = getToken();
+  res.status(HTTP_OK_STATUS).send({ token });
 });
 
 app.listen(PORT, () => {
