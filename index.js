1fbbb38f6ef1601175159f5a77b1aafd23b904a2
Aline Debastiani <debastianialine@gmail.com>
2021-05-21 16:33:44 -0300

    cumpre requisito 2

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.get('/talker', rescue(async (req, res) => {
   res.status(200).json(talkers);
 }));
 
+app.get('/talker/:id', rescue(async (req, res) => {
+  const talkers = await registeredSpeakers.getRegisteredSpeakers();
+  const talkerById = talkers.find((talker) => 
+  parseInt(talker.id, 0) === parseInt(req.params.id, 0));
+
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talkerById);
+}));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
