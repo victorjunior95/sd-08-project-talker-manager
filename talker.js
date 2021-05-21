bb5fcde3c7dd4c39f913ac7b8e99f44e6df9edb2
liisaambrosen <liisa.ambrosen@gmail.com>
2021-05-21 17:19:52 -0300

    req 2

--- talker.js
+++ talker.js
@@ -9,11 +9,21 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   const result = await fsTalker(fileToRead);
-  console.log(result);
   if (!result) {
     return res.status(401);
   }
   return res.status(200).json(result);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const results = await fsTalker(fileToRead);
+  const findResult = results.find((person) => person.id === +id);
+  console.log(findResult);
+  if (!findResult) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(findResult);
 });
 
 module.exports = router;
