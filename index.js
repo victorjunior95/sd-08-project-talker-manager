147e5b1a49f08c074c5411661550bec25d300cc1
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-21 15:19:09 -0300

    requisito 02

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await fs.readFile(`${__dirname}/talker.json`, 'utf-8')
+  .then((data) => JSON.parse(data))
+  .catch((err) => console.log(err));
+  const talker = talkers.find((person) => person.id === id);
+  if (!talker) return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  response.status(200).json(talker);
+});
+
 app.get('/talker', async (_request, response) => {
   const file = await fs.readFile(`${__dirname}/talker.json`, 'utf-8')
     .then((data) => JSON.parse(data))
     .catch((err) => console.log(err));
-  console.log(file);
   response.status(200).json(file);
 });
 
