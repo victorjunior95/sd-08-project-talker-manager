438afd206a2ab86d7bddf18a86db1984a0f62331
Fabio Ferreira <fabio35547@gmail.com>
2021-05-21 16:19:42 -0300

    Requisito 2

--- routes/talker.js
+++ routes/talker.js
@@ -4,6 +4,7 @@ const fs = require('../fs');
 
 const FILE_PATH = './talker.json';
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 const talker = express.Router();
 
@@ -15,6 +16,19 @@ talker.get('/', rescue(async (_request, response) => {
   }
 
   response.status(HTTP_OK_STATUS).send([]);
+}));
+
+talker.get('/:id', rescue(async (request, response) => {
+  const fileTalkerContent = await fs.readFile(FILE_PATH);
+
+  const talkerFound = fileTalkerContent.find(({ id }) => id === Number(request.params.id));
+
+  if (!talkerFound) {
+    return response.status(HTTP_NOT_FOUND_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  response.status(HTTP_OK_STATUS).json(talkerFound);
 }));
 
 module.exports = talker;
