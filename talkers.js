d4fc99ec2ea490e21ab096b92f12f803dde05920
Igor <igorln96@gmail.com>
2021-05-21 15:59:04 -0300

    fixing 1 and 2

--- talkers.js
+++ talkers.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const rescue = require('express-rescue');
 const fs = require('fs');
 
 const router = express.Router();
 
-const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
-
 const HTTP_OK_STATUS = 200;
 
-router.get('/:id', rescue(async (req, res) => {
+router.get('/:id', (req, res) => {
+  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
   const { id } = req.params;
 
   const talker = talkers.find((person) => person.id === Number(id));
@@ -17,13 +15,14 @@ router.get('/:id', rescue(async (req, res) => {
     res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(HTTP_OK_STATUS).json(talker);
-}));
+});
 
-router.get('/', rescue(async (_req, res) => {
+router.get('/', (_req, res) => {
+  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
   if (!talkers.length) {
     res.status(HTTP_OK_STATUS).json([]);
   }
   res.status(HTTP_OK_STATUS).json(talkers);
-}));
+});
 
 module.exports = router;
