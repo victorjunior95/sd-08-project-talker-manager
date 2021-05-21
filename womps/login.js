102b8fc2966dc9ab372ff360e7ed3da5f7475705
Bruno Martins <bruno.martins217@gmail.com>
2021-05-21 19:08:56 -0300

    Tracking file

---
+++ womps/login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const re = /.+@[A-z]+[.]com/;
+
+module.exports = (req, res) => {
+  const { email, password } = req.body;
+  const { authorization } = req.headers;
+  const stringPass = password.toString();
+  console.log(password);
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (!re.test(email)) {
+  return res.status(400).send({ 
+    message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
+  if (!stringPass) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (stringPass.length < 6) {
+  return res.status(400).send({ 
+    message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+} 
+return res.status(200).send({ token: authorization });
+};
