0133bb711acd6588aeb649f5a40b2041bc92ebff
JRGCast <joao.ralph1@gmail.com>
2021-05-21 15:59:49 -0300

    comentado path, feito exercicio 2?

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 
-const allData = fs.readFileSync('./talker.json');
+const allData = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +20,10 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// 1 - Crie o endpoint GET /talker
+// Os seguintes pontos serão avaliados:
+// O endpoint deve retornar um array com todas as pessoas palestrantes cadastradas. Devendo retornar o status 200, com o seguinte corpo:
+// Caso não exista nenhuma pessoa palestrante cadastrada o endpoint deve retornar um array vazio e o status 200.
 app.get('/talker', (_req, res) => {
   if (allData.length > 1) {
   return res.status(HTTP_OK_STATUS).send(allData);
@@ -29,4 +33,16 @@ app.get('/talker', (_req, res) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker/:id', (req, res) => {
+const idParams = req.params.id;
+const palestrantId = allData.find((element) => element.id === Number(idParams));
+
+if (palestrantId) {
+  res.status(HTTP_OK_STATUS).send(palestrantId);
+}
+res.status(404).send({
+  message: 'Pessoa palestrante não encontrada',
+});
 });
