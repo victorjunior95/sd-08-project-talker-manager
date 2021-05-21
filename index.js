72c3fdbb1c2981189f560bf4740893afa73885af
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-21 16:15:53 -0300

    requisito 03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -31,6 +32,26 @@ app.get('/talker', async (_request, response) => {
     .then((data) => JSON.parse(data))
     .catch((err) => console.log(err));
   response.status(200).json(file);
+});
+
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  const emailRegex = /.+@[A-z]+[.]com/;
+  const isEmailValid = emailRegex.test(email);
+  if (!email) return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!isEmailValid) {
+    return response.status(400)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return response.status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.toString().length < 6) {
+    return response.status(400)
+      .json({ message: 'O campo "password" deve ter pelo menos 6 caracteres' });
+  }
+  response.status(200).json({ token: crypto.randomBytes(8).toString('hex') });
 });
 
 app.listen(PORT, () => {
