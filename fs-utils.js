d4b249141f65d490b277b80636836c7302b2856e
Aline Debastiani <debastianialine@gmail.com>
2021-05-21 15:44:56 -0300

    cumpre requisito 1

---
+++ fs-utils.js
@@ -0,0 +1,12 @@
+const fs = require('fs/promises');
+
+function getRegisteredSpeakers() {
+  return fs.readFile('./talker.json', 'utf8')
+  .then((fileContent) => JSON.parse(fileContent));
+}
+
+function setRegisteredSpeakers(newSpeaker) {
+  return fs.writeFile('./talker.json', JSON.stringify(newSpeaker));
+}
+
+module.exports = { getRegisteredSpeakers, setRegisteredSpeakers };
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
+const registeredSpeakers = require('./fs-utils');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', rescue(async (req, res) => {
+  const talkers = await registeredSpeakers.getRegisteredSpeakers();
+  res.status(200).json(talkers);
+}));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
