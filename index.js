e0693531fb19271a7116502436cc0bc2e1e4ec86
Tiago <tiagaoalb@gmail.com>
2021-05-21 17:55:36 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talker = fs.readFileSync('talker.json');
+const rawTalker = JSON.parse(talker);
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +13,10 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  res.status(200).json(rawTalker);
 });
 
 app.listen(PORT, () => {
