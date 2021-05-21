0b15996580722bc4bcadc9bd9e935c5c521061fb
enicio <jessandro42@gmail.com>
2021-05-21 19:20:37 -0300

    Requisito 1.1

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send('ops');
+  response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', (_request, response) => {
   response.status(HTTP_OK_STATUS).send(talker);
+});
+
+app.get('/talker/:id', (req, response) => {
+  const { id } = req.params;
+  const person = talker.filter((element) => element.id === Number(id));
+  if (person.length === 0) {
+    return response.status(404)
+    .send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  response.status(HTTP_OK_STATUS).send(person[0]);
 });
 
 app.listen(PORT, () => {
