2068727641edda8ee62a86a6ac43fc8085382037
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-05-21 17:36:01 -0300

    Talk Manager Project - Req01

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+const talkerUtils = require('./fs-utils');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', rescue(async (_req, res) => {
+  const talkers = await talkerUtils.getTalker();
+    res.status(HTTP_OK_STATUS).json(talkers);
+}));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
