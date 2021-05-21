ee23154fefa3d899a00a2d8c0b4e79183adbb0ec
Bruno Martins <bruno.martins217@gmail.com>
2021-05-21 19:53:31 -0300

    fix*: Desafio2.js

--- womps/login.js
+++ womps/login.js
@@ -9,13 +9,17 @@ const re = /.+@[A-z]+[.]com/;
 
 module.exports = (req, res) => {
   const { email, password } = req.body;
-  const stringPass = password.toString();
+  const checkBodyLength = Object.values(req.body).length;
+  const stringPass = password;
   if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
   if (!re.test(email)) {
   return res.status(400).send({ 
     message: 'O "email" deve ter o formato "email@email.com"' }); 
 }
-  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (checkBodyLength < 2) {
+  return res.status(400).send({ 
+    message: 'O campo "password" é obrigatório' }); 
+}
   if (stringPass.length < 6) {
   return res.status(400).send({ 
     message: 'O "password" deve ter pelo menos 6 caracteres' }); 
