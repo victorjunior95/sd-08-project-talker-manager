e4d0d73ee97d358556deea1b1b5fdac4deed161d
Mateus Coury <mateuscoury@gmail.com>
2021-05-21 15:47:22 -0300

    primeiro e segundo

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-	response.status(HTTP_OK_STATUS).send();
+response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', (req, res) => {
-	if (talker.length === 0) {
-		res.status(200).send([]);
-	}
-	res.status(200).send(talker);
+if (talker.length === 0) {
+res.status(200).send([]);
+}
+res.status(200).send(talker);
 });
-
+app.get('/talker/:id', (req, res) => {
+const { id } = req.params;
+const filter = talker.find((idPeople) => idPeople.id === Number(id));
+if (!filter) {
+res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+}
+});
 app.listen(PORT, () => {
-	console.log('Online');
+console.log('Online');
 });
