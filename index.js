3fc86f5cd9cc39865837dbbc2ef76dae1ce6d85b
MEianelli <filipelouzano@gmail.com>
2021-05-21 19:45:24 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.listen(PORT, () => {
 const getAllTalkers = (_req, res, _next) => {
   const rawdata = fs.readFileSync('talker.json');
   const talkers = JSON.parse(rawdata);
+  if (!talkers.length) {
+    return res.status(200).json([]);
+  }
   res.status(200).json(talkers);
 };
 
