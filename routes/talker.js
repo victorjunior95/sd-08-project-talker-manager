f4330e410b76a03602ea130a40c7f36653ed29ed
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-05-21 15:41:17 -0300

    adicionando requisito 2

--- routes/talker.js
+++ routes/talker.js
@@ -2,8 +2,24 @@ const express = require('express');
 const readFile = require('../readFile');
 
 const SUCCESS_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
+
 const router = express.Router();
 const fileName = 'talker.json';
+
+router.get('/:id', (req, res) => {
+  readFile(fileName).then((data) => {
+    const filterTalker = data.find(
+      (talker) => talker.id === parseInt(req.params.id, 10),
+    );
+    if (filterTalker) {
+      res.status(SUCCESS_STATUS).send(filterTalker);
+    }
+    res
+      .status(NOT_FOUND_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  });
+});
 
 router.get('/', (_req, res) => {
   readFile(fileName).then((data) => {
