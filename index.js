d2a6a01b035d2384935b83dc9a2a13cc570c6760
Digo-Castro <digo_castro@outlook.com>
2021-05-21 16:27:53 -0300

    [Requisito 1] - OK

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
+const TALKER = './talker.json';
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const data = await fs.readFile(TALKER, 'utf-8');
+  return response.status(200).json(await JSON.parse(data));
 });
 
 app.listen(PORT, () => {
