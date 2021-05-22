13bab15f0ca9d355f156da5a739ca603d4cbf88f
enicio <jessandro42@gmail.com>
2021-05-21 21:58:31 -0300

    Requisito 1.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talker = require('./talker.json');
+const fs = require('fs');
+// const talker = require('./talker.json');
 
 const app = express();
 
@@ -9,18 +10,29 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function kombi() {
+ return JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
+// return 'ops';
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+  console.log(kombi());
 });
 
 app.get('/talker', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send(talker);
+  const ops = kombi();
+  if (ops.length === 0) {
+    return response.status(HTTP_OK_STATUS).send([]);
+  }
+  response.status(HTTP_OK_STATUS).send(ops);
 });
 
 app.get('/talker/:id', (req, response) => {
+  const ops = kombi();
   const { id } = req.params;
-  const person = talker.filter((element) => element.id === Number(id));
+  const person = ops.filter((element) => element.id === Number(id));
   if (person.length === 0) {
     return response.status(404)
     .send({ message: 'Pessoa palestrante não encontrada' });
