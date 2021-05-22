4d82d070c8c5db31c8dd9d3162f86d61411d0a3f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-05-22 01:35:51 -0300

    Requisito 02, status 200

--- index.js
+++ index.js
@@ -6,22 +6,45 @@ const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
+const SUCCESS = 200;
+// const CREATED = 201;
+// const BAD_REQUEST = 400;
+// const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
+// const PORT_3000 = 3000;
 const PORT = '3000';
 
 const talker = 'talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(SUCCESS).send();
 });
 
 app.get('/talker', async (req, res) => {
-  const readFile = await fs.readFile(talker);
-  if (!readFile) {
-    return res.status(HTTP_OK_STATUS).send([]);
+  const data = await fs.readFile(talker);
+
+  if (!data) {
+    return res.status(SUCCESS).send([]);
   }
-  return res.status(HTTP_OK_STATUS).send(JSON.parse(readFile));
+
+  return res.status(SUCCESS).send(JSON.parse(data));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const data = await fs.readFile(talker);
+
+  const findTalker = JSON.parse(data).find((e) => e.id === parseInt(id, 10));
+
+  if (!findTalker) {
+    return res
+      .status(NOT_FOUND)
+      .send({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(SUCCESS).send(findTalker);
 });
 
 app.listen(PORT, () => {
