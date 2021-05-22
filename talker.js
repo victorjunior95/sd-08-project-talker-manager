9e26a85d8953c317840d77d64a82847a5f4d0a13
liisaambrosen <liisa.ambrosen@gmail.com>
2021-05-22 16:46:57 -0300

    req 6

--- talker.js
+++ talker.js
@@ -1,11 +1,12 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 
 const { fsTalker, fsAdd } = require('./fsTalker.js');
 
 const authorizationMid = require('./authorization');
 
 const { nameAuth, ageAuth, talkAuth, watchedAuth, rateAuth } = require('./postMiddlewares');
+
+// const { route } = require('./login.js');
 
 const fileToRead = './talker.json';
 
@@ -32,7 +33,7 @@ router.get('/', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const results = await fsTalker(fileToRead);
-  const findResult = results.find((person) => person.id === +id);
+  const findResult = results.find((person) => person.id === Number(id));
   console.log(findResult);
   if (!findResult) {
     return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
@@ -55,5 +56,13 @@ router.post('/', [
     res.status(201).json(addTalker);
   },
 ]);
+
+router.delete('/:id', authorizationMid, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fsTalker(fileToRead);
+  const filterTalkers = talkers.filter((talker) => talker.id !== Number(id));
+  await fsAdd(JSON.stringify(filterTalkers, null, 2));
+  res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+});
 
 module.exports = router;
