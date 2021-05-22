11ab722379cd771d37eede134aa8698254844056
JRGCast <joao.ralph1@gmail.com>
2021-05-21 21:49:41 -0300

    diminuido arquivo principal, criado readFile, funcoes exportadas algumas funfam outras nao

---
+++ fsUsage/readFile.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const fsProm = require('fs').promises;
+
+const filePath = './talker.json';
+
+const syncData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+let fsPromiseData1;
+let fsPromiseData2;
+
+// metódo 1: com promise .then e .catch;
+// posso tanto fazer o assigment 
+const assigingFs1 = () => fsProm.readFile(filePath, 'utf8')
+.then((result) => { fsPromiseData1 = JSON.parse(result); }).catch((err) => console.log(err)); 
+
+console.log(fsPromiseData1);
+// quanto fazer diretamente
+const direct1 = () => fsProm.readFile(filePath, 'utf8')
+.then((result) => JSON.parse(result)).catch((err) => console.log(err)); 
+
+// método 2: com async try/catch;
+// fazendo o assigment
+const assigingFs2 = async () => { 
+try {
+ await fs.readFile(filePath, 'utf8', (err, data) => {
+   if (err) console.log(err);
+   fsPromiseData2 = JSON.parse(data);
+ });
+} catch (error) {
+  console.error(error);
+}
+};
+assigingFs2();
+
+// diretamente ??
+// const direct2 = async () => { 
+//   try {
+//    const result = fs.readFile(filePath, 'utf8', (err, data) => {
+//      if (err) console.log(err);
+//      return data;
+//    });
+//    return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   };
+//   direct2();
+
+// método 3: com promise resolve/reject
+const promiseRs1 = () => new Promise((resolve, reject) => {
+  fs.readFile('./talker.json', (err, data) => {
+    if (err) reject(err);
+    resolve(JSON.parse(data));
+  });
+}); 
+
+module.exports = {
+  syncData,
+  fsPromiseData1,
+  fsPromiseData2,
+  assigingFs1,
+  assigingFs2,
+  direct1,
+  promiseRs1,
+};
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-// const path = require('path');
-
-const allData = JSON.parse(fs.readFile('./talker.json', 'utf8'));
+const path = require('path');
+const { 
+  syncData,
+  fsPromiseData1,
+  fsPromiseData2,
+  assigingFs1,
+  assigingFs2,
+  direct1,
+  promiseRs1 } = require('./fsUsage/readFile.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,27 +18,34 @@ const PORT = '3000';
 
 // app.get('/', (_request, response) => {
 //   response.status(HTTP_OK_STATUS).sendFile(path.join(__dirname, '/index.html'));
-// }); para brincar dps
+// }); // para brincar dps
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send('Olá Mundo');
-});
-
-app.get('/talker', (_req, res) => {
-  if (allData.length > 0) {
-  return res.status(HTTP_OK_STATUS).send(allData);
-  }
-  return res.status(HTTP_OK_STATUS).send([]);
+app.get('/', async (_request, response) => {
+  response.status(HTTP_OK_STATUS).send({
+    message: 'Olá Mundo',
+    sync1: syncData,
+     assigment1: assigingFs1(),
+     assigment2: fsPromiseData2,
+     direct1: await direct1(), 
+    //  direct2: await direct2(),
+    promise: await promiseRs1(),
+    });
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
+app.get('/talker', (_req, res) => {
+  if (syncData.length > 0) {
+  return res.status(HTTP_OK_STATUS).send(syncData);
+  }
+  return res.status(HTTP_OK_STATUS).send([]);
+});
 
 app.get('/talker/:id', (req, res) => {
-const idParams = req.params.id;
-const palestrantId = allData.find((element) => element.id === Number(idParams));
+const idParams = Number(req.params.id);
+const palestrantId = syncData.find((element) => element.id === idParams);
 
 if (!palestrantId) {
   res.status(404).send({
@@ -44,5 +56,6 @@ res.status(HTTP_OK_STATUS).send(palestrantId);
 });
 
 app.post('/login', (req, res) => {
-res.status(HTTP_OK_STATUS).send(req.body);
+  const { name } = req.body;
+res.status(HTTP_OK_STATUS).send({ message: name });
 });
