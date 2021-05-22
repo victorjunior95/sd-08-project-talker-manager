98faf0f998a12d5d5ab56a2ff7e0aab0e6bf2412
Ruan Miguel <ruanmiguelps@gmail.com>
2021-05-22 13:07:12 -0300

    delteTalker implementado

--- index.js
+++ index.js
@@ -73,6 +73,18 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(finder);
 });
 
+app.delete('/talker/:id', tokenValidator, tokenValid, async (req, res) => {
+  const {id} = req.params
+  const talkers = await fs.readFile(FILEPATH, 'utf8');
+  const jason = await JSON.parse(talkers);
+  if (jason.some((item) => item.id === +id)) {
+    const filtered = jason.filter((item) => item.id !== +id);
+    const writer = await fs.writeFile(FILEPATH, JSON.stringify(filtered));
+    console.log(writer);
+    return res.status(200).json({ message: "Pessoa palestrante deletada com sucesso"  });
+  }
+})
+
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   const validadeRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
