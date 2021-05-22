54dd40e6f05b5abe7ac9f7b54381840527f15ff5
Ruan Miguel <ruanmiguelps@gmail.com>
2021-05-22 12:59:16 -0300

    edit talker adicionado

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const tokenValid = require('./tokenValid');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const FILEPATH = 'talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -24,8 +25,8 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (req, res) => {
-  const talkers = await fs.readFile('talker.json', 'utf8');
-  const jason = await JSON.parse(talkers);
+  const talker = await fs.readFile(FILEPATH, 'utf8');
+  const jason = await JSON.parse(talker);
   res.status(200).json(jason);
 });
 
@@ -33,18 +34,35 @@ app.post('/talker', tokenValidator, tokenValid, nameValidador,
  ageValidador, talkerValidator, talkValidator, rateValidador, dateValidador, async (req, res) => {
   const { name, age, talk } = req.body;
   
-  const talkers = await fs.readFile('talker.json', 'utf8');
-  const jason = await JSON.parse(talkers);
+  const talkerss = await fs.readFile(FILEPATH, 'utf8');
+  const jason = await JSON.parse(talkerss);
   const newCommer = { name, age, id: jason.length + 1, talk };
   const add = await jason.concat(newCommer);
-  const writer = await fs.writeFile('talker.json', JSON.stringify(add));
+  const writer = await fs.writeFile(FILEPATH, JSON.stringify(add));
   console.log(writer);
   return res.status(201).json(newCommer);
 });
 
+app.put('/talker/:id', tokenValidator, tokenValid, nameValidador,
+ageValidador, talkerValidator, talkValidator, rateValidador, dateValidador, async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+
+  const talkers = await fs.readFile(FILEPATH, 'utf8');
+  const jason = await JSON.parse(talkers);
+  if (jason.some((item) => item.id === +id)) {
+    const filtered = jason.filter((item) => item.id !== +id);
+    const newCommer = { name, age, id: +id, talk };
+    const add = await filtered.concat(newCommer);
+    const writer = await fs.writeFile(FILEPATH, JSON.stringify(add));
+    console.log(writer);
+    return res.status(200).json(newCommer);
+  }
+});
+
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
-  const talkers = await fs.readFile('talker.json', 'utf8');
+  const talkers = await fs.readFile(FILEPATH, 'utf8');
   const jason = await JSON.parse(talkers);
   const finder = jason.find((item) => item.id === +id);
 
