d7a56af83617a47cb1df5aa786dcc7e4845cc700
Bruno Martins <bruno.martins217@gmail.com>
2021-05-22 12:18:23 -0300

    Desafio 4 OK

--- womps/createTalker.js
+++ womps/createTalker.js
@@ -13,7 +13,7 @@ function checkToken(authorization) {
 
 function checkName(name) {
   if (!name) throw new Error('O campo "name" é obrigatório');
-  if (name.length < 3) throw new Error('O "name" ter pelo menos 3 caracteres');
+  if (name.length < 3) throw new Error('O "name" deve ter pelo menos 3 caracteres');
 }
 
 function checkAge(age) {
@@ -21,14 +21,35 @@ function checkAge(age) {
   if (age < 18) throw new Error('A pessoa palestrante deve ser maior de idade');
 }
 
-function checkTalk(talk) {
+function checkWathedRate(talk) {
   const { watchedAt, rate } = talk;
   const re = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
   if (!re.test(watchedAt)) throw new Error('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"');
   if (rate < 1 || rate > 5) throw new Error('O campo "rate" deve ser um inteiro de 1 à 5');
-  if (!talk) {
+}
+
+function checkRate(talk) {
+  if (!talk.rate) {
     throw new Error('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'); 
 }
+}
+
+function checkWatchedAt(talk) {
+  if (!talk.watchedAt) {
+    throw new Error('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios');
+  }
+}
+
+function checkTalk(req) {
+  if (!req.body.talk) {
+    throw new Error('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'); 
+  }
+}
+
+function checkTalkWatchedRate(talk, req) {
+  checkTalk(req);
+  checkRate(talk);
+  checkWatchedAt(talk);
 }
 
 module.exports = (req, res) => {
@@ -39,7 +60,8 @@ module.exports = (req, res) => {
     checkToken(authorization);
     checkName(name);
     checkAge(age);
-    checkTalk(talk);
+    checkTalkWatchedRate(talk, req);
+    checkWathedRate(talk);
     data.push({ name, age, id: data.length + 1, talk });
     fs.writeFileSync('talker.json', JSON.stringify(data));
     res.status(201).json(data[data.length - 1]);
