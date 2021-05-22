f78676d85c8eb73e317ad22e1ea9802770afe88b
Bruno Martins <bruno.martins217@gmail.com>
2021-05-22 16:13:46 -0300

    refactor: Desafio 4

--- womps/createTalker.js
+++ womps/createTalker.js
@@ -21,13 +21,6 @@ function checkAge(age) {
   if (age < 18) throw new Error('A pessoa palestrante deve ser maior de idade');
 }
 
-function checkWathedRate(talk) {
-  const { watchedAt, rate } = talk;
-  const re = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
-  if (!re.test(watchedAt)) throw new Error('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"');
-  if (rate < 1 || rate > 5) throw new Error('O campo "rate" deve ser um inteiro de 1 à 5');
-}
-
 function checkRate(talk) {
   if (!talk.rate) {
     throw new Error('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'); 
@@ -40,34 +33,44 @@ function checkWatchedAt(talk) {
   }
 }
 
+function checkWatchedRate(talk) {
+  const { watchedAt, rate } = talk;
+  const re = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/; // https://stackoverflow.com/questions/5465375/javascript-date-regex-dd-mm-yyyy
+  if (!re.test(watchedAt)) throw new Error('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"');
+  if (rate < 1 || rate > 5) throw new Error('O campo "rate" deve ser um inteiro de 1 à 5');
+}
+
 function checkTalk(req) {
   if (!req.body.talk) {
     throw new Error('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'); 
   }
 }
-
-function checkTalkWatchedRate(talk, req) {
+function checks(req) {
+  const { authorization } = req.headers;
+  const { name, age, talk } = req.body;
+  checkToken(authorization);
+  checkName(name);
+  checkAge(age);
   checkTalk(req);
-  checkRate(talk);
   checkWatchedAt(talk);
+  checkWatchedRate(talk);
+  checkRate(talk);
 }
+// function checkTalkWatchedRate(talk, req) {
+//   checkTalk(req);
+//   checkRate(talk);
+//   checkWatchedAt(talk);
+// }
 
 module.exports = (req, res) => {
   const data = JSON.parse(fs.readFileSync(meuArquivo, 'utf-8'));
-  const { authorization } = req.headers;
   const { name, age, talk } = req.body;
   try {
-    checkToken(authorization);
-    checkName(name);
-    checkAge(age);
-    checkTalkWatchedRate(talk, req);
-    checkWathedRate(talk);
+    checks(req);
     data.push({ name, age, id: data.length + 1, talk });
     fs.writeFileSync('talker.json', JSON.stringify(data));
     res.status(201).json(data[data.length - 1]);
-    console.log('ok');
   } catch (error) {
-    console.log('deuruim');
     if (error.message.includes('Token')) return res.status(401).json({ message: error.message });
     return res.status(400).json({ message: error.message });
   }
