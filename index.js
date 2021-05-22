b184a5f8e4f6e5ce4e893eb04de998171e848c4d
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-05-22 02:23:47 -0300

    Requisito 03, status 200

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const randtoken = require('rand-token');
 // const path = require('path');
 
 const app = express();
@@ -8,21 +9,23 @@ app.use(bodyParser.json());
 
 const SUCCESS = 200;
 // const CREATED = 201;
-// const BAD_REQUEST = 400;
+const BAD_REQUEST = 400;
 // const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
 // const PORT_3000 = 3000;
 const PORT = '3000';
 
-const talker = 'talker.json';
+const TALKER_FILE = 'talker.json';
+const VALID_EMAIL_REGEX = /\S+@\S+\.\S+/;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+// Requisito 01
 app.get('/talker', async (req, res) => {
-  const data = await fs.readFile(talker);
+  const data = await fs.readFile(TALKER_FILE);
 
   if (!data) {
     return res.status(SUCCESS).send([]);
@@ -31,10 +34,11 @@ app.get('/talker', async (req, res) => {
   return res.status(SUCCESS).send(JSON.parse(data));
 });
 
+// Requisito 02
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
 
-  const data = await fs.readFile(talker);
+  const data = await fs.readFile(TALKER_FILE);
 
   const findTalker = JSON.parse(data).find((e) => e.id === parseInt(id, 10));
 
@@ -45,6 +49,45 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   return res.status(SUCCESS).send(findTalker);
+});
+
+const validateEmail = (email) => {
+  if (!email || email === '') {
+    return { message: 'O campo "email" é obrigatório' };
+  }
+  if (!VALID_EMAIL_REGEX.test(email)) {
+    return { message: 'O "email" deve ter o formato "email@email.com"' };
+  }
+  return true;
+};
+
+const validatePassword = (password) => {
+  if (!password || password === '') {
+    return { message: 'O campo "password" é obrigatório' };
+  }
+  if (password.toString().length < 6) {
+    return { message: 'O "password" deve ter pelo menos 6 caracteres' };
+  }
+  return true;
+};
+
+// Requisito 03
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = validatePassword(password);
+
+  if (typeof isEmailValid === 'object') {
+    return res.status(BAD_REQUEST).send(isEmailValid);
+  }
+
+  if (typeof isPasswordValid === 'object') {
+    return res.status(BAD_REQUEST).send(isPasswordValid);
+  }
+
+  const token = randtoken.generate(16);
+
+  return res.status(SUCCESS).send({ token: `${token}` });
 });
 
 app.listen(PORT, () => {
