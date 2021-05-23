dad530c033cd19b9b1b2f2c668d9823981167b16
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-23 11:06:59 -0300

    corrige middleware de token e name

--- middlewares/checkNameMiddleware.js
+++ middlewares/checkNameMiddleware.js
@@ -1,7 +1,7 @@
 module.exports = (request, response, next) => {
   const { name } = request.body;
   if (!name) return response.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.lenth < 3) {
+  if (name.length < 3) {
     return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   next();
--- middlewares/checkTokenMiddleware.js
+++ middlewares/checkTokenMiddleware.js
@@ -1,7 +1,7 @@
 module.exports = (request, response, next) => {
-  const { token } = request.headers.authorization;
+  const token = request.headers.authorization;
   if (!token) return response.status(401).json({ message: 'Token não encontrado' });
-  if (typeof token !== 'string' || token.length !== 16) {
+  if (typeof token !== 'string' || token.length !== 16 || token === '') {
     return response.status(401).json({ message: 'Token inválido' });
   }
   next();
