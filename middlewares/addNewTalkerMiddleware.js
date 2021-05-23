b062c2f4b410dcefdcf0d5807adc08d0cf622eda
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-23 11:46:48 -0300

    conclui requisito 04

--- middlewares/addNewTalkerMiddleware.js
+++ middlewares/addNewTalkerMiddleware.js
@@ -2,14 +2,14 @@ const fs = require('fs').promises;
 const talkersData = require('../talker.json');
 
 module.exports = async (request, response) => {
-  console.log('talkers data', talkersData);
+  const allTalkers = [...talkersData];
   const talker = request.body;
   const newTalker = {
-    id: talkersData[talkersData.length - 1].id + 1,
+    id: allTalkers[allTalkers.length - 1].id + 1,
     ...talker,
   };
-  talkersData.push(newTalker);
-  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(talkersData))
+  allTalkers.push(newTalker);
+  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(allTalkers))
     .then(() => response.status(201).json(newTalker))
     .catch((err) => console.log(err));
 };
