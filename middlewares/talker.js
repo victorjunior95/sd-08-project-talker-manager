1e2be5bde5d37deda117999bcfcc23b644d94e07
albertassi88 <albertassi88@gmail.com>
2021-05-23 16:01:51 -0300

    Alteracoes no exercicio 1

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -5,6 +5,6 @@ const arquivo = fs.readFileSync('talker.json', 'utf8');
 
 module.exports = rescue(async (req, res) => {
     const file = await arquivo;
-    if (file.length === 0) return res.status(200).json([]);
+    if (!file.length === 0) return res.status(200).json([]);
     return res.status(200).json(JSON.parse(file));
 });
