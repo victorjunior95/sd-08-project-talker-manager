cc6b07d474b4ee0f8c870b68bb7e65ba5bc05228
albertassi88 <albertassi88@gmail.com>
2021-05-23 16:58:39 -0300

    Alteracoes no exercicio 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 
 const middlewares = require('./middlewares');
 
@@ -8,13 +9,16 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', middlewares.talker);
+app.get('/talker', rescue(async (req, res) => {
+  const file = await middlewares.talker();
+  if (file.length === 0) return res.status(200).json([]);
+  return res.status(200).json(file);
+ }));
 
 app.listen(PORT, () => {
   console.log('Online');
--- middlewares/talker.js
+++ middlewares/talker.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
-const rescue = require('express-rescue');
 
 const arquivo = fs.readFileSync('talker.json', 'utf8');
 
-module.exports = rescue(async (req, res) => {
-    const file = await arquivo;
-    if (file) return res.status(200).json(JSON.parse(file));    
-    return res.status(200).json([]);
-});
+module.exports = () => JSON.parse(arquivo);
