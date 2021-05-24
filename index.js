9a189d13c5b1962745b98425d6aeed06b39e137c
amandaos <amanda.775.souza@gmail.com>
2021-05-24 19:12:44 -0300

    feature: cria endpoint /login

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+function validateEmail(email) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+// https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+function geraToken(tamanho) {
+  let token = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < tamanho; i = +1) {
+    token += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return token;
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -20,12 +36,26 @@ app.get('/talker', (req, res) => {
 });
 
 app.get('/talker/:id', (req, res) => {
-     const { id } = req.params;
-     const talkers = JSON.parse(fs.readFileSync('./talker.json'));
-     const returnTalk = talkers.find((talker) => talker.id === parseInt(id, 10));
-     if (!returnTalk) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-    res.status(HTTP_OK_STATUS).send(returnTalk);
-  });
+  const { id } = req.params;
+  const talkers = JSON.parse(fs.readFileSync('./talker.json'));
+  const returnTalk = talkers.find((talker) => talker.id === parseInt(id, 10));
+  if (!returnTalk) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK_STATUS).send(returnTalk);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (validateEmail(email)) {
+    res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (password.toString().length < 6) {
+    res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const token = geraToken();
+  res.status(HTTP_OK_STATUS).status.send({ token });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
