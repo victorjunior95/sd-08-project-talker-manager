a541000bc3d15150f33f9234c2d86430993f20fc
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-05-24 20:51:48 -0300

    Talk Manager Project - Req04(fixed?4)

--- middlewares/addTalker.js
+++ middlewares/addTalker.js
@@ -1,20 +1,12 @@
 const fs = require('fs');
-const talkers = require('../talker.json');
+const { getTalker } = require('../fs-utils');
 
 // referencia turma 07 - aula 26.05
 const addTalker = async (req, res) => {
-  const size = talkers.length;
-  talkers[size] = {
-        name: req.body.name,
-        age: req.body.age,
-        id: `${size + 1}`,
-        talk: {
-          watchedAt: req.body.talk.watchedAt,
-          rate: req.body.talk.rate,
-        },
-  };
   try {
+    const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
     const newTalker = req.body;
+    newTalker.id = talkers.length + 1;
     talkers.push(newTalker);
   await fs.promises.writeFile(`${__dirname}/../talker.json`, JSON.stringify(talkers));
   res.status(201).json(newTalker);
