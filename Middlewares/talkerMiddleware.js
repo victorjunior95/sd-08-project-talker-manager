d40cab2c940d5e675c65b4051327e5ee32b5836e
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-24 15:18:19 -0300

    req1 ok

---
+++ Middlewares/talkerMiddleware.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const dbTalker = fs.readFileSync('talker.json', 'utf8');
+
+const talkerMiddleware = (_req, res) => (dbTalker.length === 0 
+  ? res.status(200).send(JSON.parse([])) : res.status(200).send(JSON.parse(dbTalker)));
+
+module.exports = talkerMiddleware;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkerMidware = require('./Middlewares/talkerMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
+console.log(talkerMidware);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -11,6 +13,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', talkerMidware);
 
 app.listen(PORT, () => {
   console.log('Online');
