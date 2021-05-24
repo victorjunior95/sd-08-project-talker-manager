45455a25d18e6ae976fe382d3c0b29111e822484
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-24 17:58:52 -0300

    fix: challenge 1

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function challengeOne(_req, res) {
-  res.status(200).send(fs.readFileSync('./talker.json'));
+  res.status(200).send(JSON.parse(fs.readFileSync('./talker.json')));
 }
 
 module.exports = challengeOne;
