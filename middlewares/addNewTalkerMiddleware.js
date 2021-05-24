32ddc883b6986da7dbaea1fff81725b3e2776b78
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-05-24 16:02:56 -0300

    requisito 05 - refatora leitura do arquivo json

--- middlewares/addNewTalkerMiddleware.js
+++ middlewares/addNewTalkerMiddleware.js
@@ -1,15 +1,17 @@
 const fs = require('fs').promises;
-const talkersData = require('../talker.json');
+// const talkersData = require('../talker.json');
 
 module.exports = async (request, response) => {
-  const allTalkers = [...talkersData];
+  const allTalkers = await fs.readFile(`${__dirname}/../talker.json`, 'utf-8')
+    .then((res) => JSON.parse(res))
+    .catch((err) => console.log(err));
   const talker = request.body;
   const newTalker = {
     id: allTalkers[allTalkers.length - 1].id + 1,
     ...talker,
   };
   allTalkers.push(newTalker);
-  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(allTalkers))
-    .then(() => response.status(201).json(newTalker))
-    .catch((err) => console.log(err));
+  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(allTalkers));
+  return response.status(201).json(newTalker);
+    // .catch((err) => console.log(err));
 };
--- middlewares/updateTalkerByIdMiddleware.js
+++ middlewares/updateTalkerByIdMiddleware.js
@@ -1,18 +1,20 @@
 const fs = require('fs').promises;
-const talkersData = require('../talker.json');
+// const talkersData = require('../talker.json');
 
-module.exports = async (request, response, next) => {
+module.exports = async (request, response) => {
   const { id } = request.params;
   const newId = parseInt(id, 10);
-  const allTalkers = [...talkersData];
+  const allTalkers = await fs.readFile(`${__dirname}/../talker.json`, 'utf-8')
+    .then((res) => JSON.parse(res))
+    .catch((err) => console.log(err));
   const talker = request.body;
   const newTalker = { id: newId, ...talker };
   const updatedTalkers = allTalkers.map((speaker) => {
     if (speaker.id === newId) return newTalker;
     return speaker;
   });
-  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(updatedTalkers))
-  .then(() => response.status(200).json(newTalker))
-  .catch((err) => console.log(err));
-  next();
+  console.log('updatedTalkers', updatedTalkers);
+  await fs.writeFile(`${__dirname}/../talker.json`, JSON.stringify(updatedTalkers));
+  return response.status(200).json(newTalker);
+  // .catch((err) => console.log(err));
 };
