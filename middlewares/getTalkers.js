0cb6d6c7d603db444c2299f7997d9427483c420f
caiomzrocha <caiomzrocha@gmail.com>
2021-05-24 15:41:25 -0300

    [REQ-01][FIX] getTalkers retorna um array no lugar de um objeto.

--- middlewares/getTalkers.js
+++ middlewares/getTalkers.js
@@ -4,7 +4,7 @@ const getTalkers = (req, res, _) => {
   try {
     const fileContent = fs.readFileSync('./talker.json', 'utf8');
     const talkers = JSON.parse(fileContent) || [];
-    return res.status(200).json({ json: talkers });
+    return res.status(200).json(talkers);
   } catch (err) {
     return res.status(500).send(err);
   }
