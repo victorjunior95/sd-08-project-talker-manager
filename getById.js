305f577ae285c9dc38c20d44484e531be8f77215
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-24 19:27:36 -0300

    fix: challenge 2

--- getById.js
+++ getById.js
@@ -3,14 +3,13 @@ const readSync = require('./helpers/readSync');
 function getById(req, res) {
   const { id } = req.params;
   const idNumber = Number(id);
-  const errorMessage = readSync('./errorMessage.json');
   const file = readSync('./talker.json');
-  const personById = file.filter((item) => item.id === idNumber);
+  const personById = file.find((item) => item.id === idNumber);
 
-  if (personById.length > 0) {
-    res.status(200).send(file[0]);
+  if (personById) {
+    res.status(200).send(personById);
   } else {
-    res.status(404).send(errorMessage);
+    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
   } 
 }
 
