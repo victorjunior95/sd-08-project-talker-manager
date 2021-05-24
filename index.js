d03df4ca2169008ca6524b49d248f7330edb20f9
Victor Breno <victor.brns2@gmail.com>
2021-05-24 17:20:41 -0300

    inicio do projeto

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  fs.readFile('talker.json')
+    .then((data) => {
+      console.log(JSON.parse(data));
+      res.status(200).send(JSON.parse(data));
+    })
+    .catch(() => {
+      res.status(200).send([]);
+    });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
