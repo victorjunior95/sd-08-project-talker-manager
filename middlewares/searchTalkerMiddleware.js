9c74b1de345a519f084813a9db45561ec1a57955
Digo-Castro <digo_castro@outlook.com>
2021-05-24 15:34:50 -0300

    [Projeto Talker Manager] - OK

--- middlewares/searchTalkerMiddleware.js
+++ middlewares/searchTalkerMiddleware.js
@@ -4,17 +4,13 @@ const { TALKER } = require('../services');
 module.exports = async (request, response, _next) => {
   const talkersList = await getData(TALKER);
   let query = request.query.q;
-  if (!query) {
-    query = '';
-  }
+  if (!query) { query = ''; }
   console.log(request.query.q);
   const regex = new RegExp(`${query}`);
   console.log(regex);
   const list = [];
   await talkersList.forEach((talker) => {
-    if (regex.test(talker.name)) {
-      list.push(talker);
-    }
+    if (regex.test(talker.name)) { list.push(talker); }
   });
   return response.status(200).json(list);
 };
