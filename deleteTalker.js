3ca068d372f5350c4fdd97e72f9b3c441da18c37
Mateus Coury <mateuscoury@gmail.com>
2021-05-24 16:54:27 -0300

    correcao1

--- deleteTalker.js
+++ deleteTalker.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 
 const getTalkers = () => JSON.parse(fs.readFileSync('talker.json', 'utf8'));
+
 module.exports = (req, res, _next) => {
-  const { id } = req.params;
-  const talkers = getTalkers();
-  const deleteTalker = talkers.filter((e) => e.id !== Number(id));
-  fs.writeFileSync('talker.json', JSON.stringify(deleteTalker));
-  res
-  .status(200)
-  .json({ message: 'Pessoa palestrante deletada com sucesso' });
+  try {
+    const { id } = req.params;
+    const talkers = getTalkers();
+    const deleteTalker = talkers.filter((e) => e.id !== Number(id));
+    fs.writeFileSync('talker.json', JSON.stringify(deleteTalker));
+    res
+    .status(200)
+    .json({ message: 'Pessoa palestrante deletada com sucesso' });
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
 };
