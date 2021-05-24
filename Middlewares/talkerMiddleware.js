423d90cc027c35f31623766d0c8bb3160a98f118
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-24 15:41:03 -0300

    req1 take3

--- Middlewares/talkerMiddleware.js
+++ Middlewares/talkerMiddleware.js
@@ -4,6 +4,6 @@ const dbTalker = fs.readFileSync('talker.json', 'utf8');
 
 const talkerMiddleware = (_req, res) => 
 (dbTalker.length === 0 
-  ? res.status(200).send(JSON.parse([])) : res.status(200).send(JSON.parse(dbTalker)));
+  ? (res.status(200).send(JSON.parse([]))) : (res.status(200).send(JSON.parse(dbTalker))));
 
 module.exports = talkerMiddleware;
