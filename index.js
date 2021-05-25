cbece8d85fa80148c9edb08df2dca9369fb54d45
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-25 15:26:54 -0300

    Updating...

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const fs = require("fs").promises;
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const { HTTP_OK_STATUS, PORT } = require("./src/common/defs");
 
+const file = "talker.json"; // mock or data
+
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (_request, response) => {
-  response.status(HTTP_OK_STATUS).send({});
+  response.status(HTTP_OK_STATUS).send("Hello World!");
+});
+
+app.get("/talker", async (_request, response) => {
+  const mockData = await fs.readFile(file);
+  if (!mockData) {
+    return response.status(HTTP_OK_STATUS).send([]);
+  }
+  return response.status(HTTP_OK_STATUS).send(JSON.parse(mockData));
 });
 
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log("Online in | http://localhost:3000/");
 });
