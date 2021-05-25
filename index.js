f9cc185eab8cfdb932bae5991f22937300780495
Clênio Galvão <clenio.cgm@gmail.com>
2021-05-24 21:01:46 -0300

    Resolve quesito 1

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talker = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
 
+// app.use(function (err, req, res, next) {
+//   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+// });
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  if (!talker) {
+    return res.status(200).json([]);
+  }
+  res.status(200).json(talker);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
