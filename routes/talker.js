75010b889a2cf5ca2ef0e084bb1b41a7e192ebac
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-05-25 16:37:42 -0300

    requisito 2

--- routes/talker.js
+++ routes/talker.js
@@ -15,4 +15,19 @@ app.get('/', async (_req, res) => {
     }
   });
 
+  app.get('/:id', async (req, res) => {
+    try {
+      const array = await readFile(data);
+      const filterById = array.find((elememt) => elememt.id === parseInt(req.params.id, 10));
+
+      if (!filterById) {
+        res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+      } else {
+        res.status(200).json(filterById);
+      }
+    } catch (error) {
+        res.status(500).json({ message: 'fatal error' });
+    }
+  });
+
 module.exports = app;
