e8c78ecfa806b9edfbe4dabef07a8a703ff358cc
Victor Miwa <victoryukimiwa@hotmail.com>
2021-05-25 14:01:47 -0300

    Fix code climate

--- fileFunctions.js
+++ fileFunctions.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-async function readFile() {
-  try {
-    const response = await fs.promises.readFile('./talker.json', 'utf-8');
-    return response;
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+// async function readFile() {
+//   try {
+//     const response = await fs.promises.readFile('./talker.json', 'utf-8');
+//     return response;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 
-async function writeFile(message) {
-  try {
-    await fs.promises.writeFile('./talker.json', JSON.stringify(message));
-    console.log('Salvo com sucesso!!!');
-  } catch (error) {
-    console.log(error.menssage);
-  }
-}
+// async function writeFile(message) {
+//   try {
+//     await fs.promises.writeFile('./talker.json', JSON.stringify(message));
+//     console.log('Salvo com sucesso!!!');
+//   } catch (error) {
+//     console.log(error.menssage);
+//   }
+// }
 
-readFile();
+// readFile();
