8c94b36da28df5ebbf3a82e79f06d228826dccdb
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-25 15:59:14 -0300

    Updating.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const fs = require("fs").promises;
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { HTTP_OK_STATUS, PORT } = require("./src/common/defs");
+const {
+  HTTP_OK_STATUS,
+  HTTP_NOT_FOUND_STATUS,
+  PORT,
+} = require("./src/common/defs");
 
-const file = "talker.json"; // mock or data
+const file = "./talker.json";
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +23,20 @@ app.get("/talker", async (_request, response) => {
     return response.status(HTTP_OK_STATUS).send([]);
   }
   return response.status(HTTP_OK_STATUS).send(JSON.parse(mockData));
+});
+
+app.get("/talker/:id", async (_request, response) => {
+  const mockData = await fs.readFile(file);
+  const { id } = _request.params;
+  const talker = JSON.parse(mockData).find(
+    (talker) => talker.id === parseInt(id)
+  );
+  if (!talker) {
+    return response.status(HTTP_NOT_FOUND_STATUS).send({
+      message: "Pessoa palestrante não encontrada",
+    });
+  }
+  return response.status(HTTP_OK_STATUS).send(talker);
 });
 
 app.listen(PORT, () => {
--- src/common/defs.js
+++ src/common/defs.js
@@ -1,7 +1,9 @@
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = "3000";
 
 module.exports = {
   HTTP_OK_STATUS,
+  HTTP_NOT_FOUND_STATUS,
   PORT,
 };
