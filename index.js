a28a63a50bf1e106b00feca8f6d27470aca68436
Rita Jeveaux <ritajeveaux@gmail.com>
2021-05-25 20:44:36 -0300

    Login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const data = fs.readFileSync('./talker.json', 'utf8');
+const login = require('./utils');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,22 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Requisito 02 -----------------------------------------------------
+app.get('/talker/:id', (req, res) => {
+  try {
+      const { id } = req.params;
+      const findTalker = data.find((talker) => talker.id === id);
+      console.log(findTalker); 
+      if (findTalker) {
+      res.status(200).send(findTalker);
+    } else {
+      res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+    }
+  } catch (err) {
+    return res.status(500).send({ err });
+  }
 });
 
 // Requisito 01 -----------------------------------------------------
@@ -26,22 +43,16 @@ app.get('/talker', (_req, res) => {
 // }
 });
 
-// Requisito 02 -----------------------------------------------------
-app.get('/talker/:id', (req, res) => {
-  try {
-      const id = parseInt(req.params.id, 10);
-      const findTalker = data.find((talker) => talker.id === id);
-        if (findTalker) {
-      res.status(200).send(findTalker);
-    } else {
-      res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-    }
-  } catch (err) {
-    return res.status(500).send({ err });
-  }
-});
-
 // Requisito 03 -----------------------------------------------------
+// app.post('/login', (_req, res) => {
+//   if (data) return res.status(200).json(JSON.parse(data));
+//   res.status(200).send([]);
+//    try {
+//      res.status(200).send(data);  
+//    } catch (err) {
+//     throw new Err(err);
+// }
+// });
 
 app.listen(PORT, () => {
   console.log('Online');
