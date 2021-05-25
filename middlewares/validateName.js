13d31f3a47620bee482641b375a5f19fc69eab0b
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-25 19:03:56 -0300

    fix: token and name validation

--- middlewares/validateName.js
+++ middlewares/validateName.js
@@ -1,6 +1,5 @@
 const validateName = (req, res, next) => {
   const { name } = req.body;
-  console.log(name);
   if (!name) {
     return res.status(400).send({ message: 'O campo "name" é obrigatório' });
   }
