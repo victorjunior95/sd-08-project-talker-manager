f5efea5452c61a9e54e3129842df424cec048d30
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-25 19:06:03 -0300

    fix: age validation

--- middlewares/validateAge.js
+++ middlewares/validateAge.js
@@ -3,7 +3,7 @@ const validateAge = (req, res, next) => {
   if (!age) {
     return res.status(400).send({ message: 'O campo "age" é obrigatório' });
   }
-  if (age >= 18) {
+  if (age < 18) {
     return res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
   next();
