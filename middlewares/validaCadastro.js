a3c5869d80d0436fe5303836b7f7593677454cbc
amandaos <amanda.775.souza@gmail.com>
2021-05-26 11:40:19 -0300

    test

--- middlewares/validaCadastro.js
+++ middlewares/validaCadastro.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const validaCadastro = (req, res) => {
   const talker = JSON.parse(fs.readFileSync('./talker.json'));
   const reqbody = { id: talker.length + 1, ...req.body };
-  const newTalker = JSON.stringify([...talker, ...reqbody]);
+  const newTalker = JSON.stringify([...talker, reqbody]);
   fs.writeFileSync('./talker.json', newTalker);
   res.status(201).json(reqbody);
 };
