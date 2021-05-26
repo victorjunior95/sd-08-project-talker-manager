cb08f0e986b90acec9a06da6706dbe4dc325fb37
amandaos <amanda.775.souza@gmail.com>
2021-05-26 15:11:15 -0300

    eu nao aguento mais corrigir o req 4

--- middlewares/validaNome.js
+++ middlewares/validaNome.js
@@ -3,7 +3,7 @@ const validaNome = (req, res, next) => {
   if (!name) {
     res.status(400).json({ message: 'O campo "name" é obrigatório' });
   } 
-  if (name.length > 3) {
+  if (name.length < 3) {
     res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   } 
   next();
