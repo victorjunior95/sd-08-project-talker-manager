c8a9a438dabeffee6dca632937d8ba545be4feaf
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-05-26 13:08:21 -0300

    fix: lint issue

--- middlewares/postTalker.js
+++ middlewares/postTalker.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises');
+const { promises } = require('fs');
 const createTalker = require('../helpers/createTalker');
 const addTalker = require('../helpers/addTalker');
 
@@ -7,7 +7,7 @@ const nomeDoArquivo = 'talker.json';
 const postTalker = (req, res) => {
   const obj = createTalker(req.body);
   const content = addTalker(obj);
-  fs.writeFile(nomeDoArquivo, content)
+  promises.writeFile(nomeDoArquivo, content)
     .then(() => res.status(201).send(obj))
     .catch((err) => console.log(err.message));
 };
