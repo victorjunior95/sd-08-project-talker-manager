dab5839a2e775f15357416c1c622c15246135636
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-05-26 12:15:57 -0300

    requisito 3 - retorna token de 16 caracteres caso receba email e password valido

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = '3000';
 
 const rescue = require('express-rescue');
 const talkersFile = require('./middleware/fs-readFile');
+const crypto = require('crypto');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -31,6 +32,25 @@ rescue(async (req, res, _next) => {
   }
   res.status(200).json(talker);
 }));
+
+app.post('/login', (req, res, _next) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  if (!email) {
+    return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+  }
+  if (!password) {
+    return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+  }
+  if (!email.match(/(.+@.+\.com)(\.br)?/)) {
+    return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres" });
+  }
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
