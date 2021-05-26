cb00f248dbbc666af20091d636e11fe88378bc8a
Bruno Wesley <bruno.wesley77@gmail.com>
2021-05-26 17:46:46 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,11 +8,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+// Requisito 1
+app.get('/talker', (_req, res) => {
+  fs.readFile('./talker.json', (_err, data) => {
+    res.status(HTTP_OK_STATUS).send(JSON.parse(data));
+  });
 });
