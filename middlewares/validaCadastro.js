23a83faf997794a9ce451cf7846d9f366f9e563c
amandaos <amanda.775.souza@gmail.com>
2021-05-26 11:29:50 -0300

    fix: corrige digitacao

--- middlewares/validaCadastro.js
+++ middlewares/validaCadastro.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 const validaCadastro = (req, res) => {
   const talker = JSON.parse(fs.readFileSync('./talker.json'));
-  const body = { id: talker.length + 1, ...req.body };
-  const newTalker = JSON.stringify([...talker, ...body]);
+  const reqbody = { id: talker.length + 1, ...req.body };
+  const newTalker = JSON.stringify([...talker, ...reqbody]);
   fs.writeFileSync('./talker.json', newTalker);
-  res.status(201).json(body);
+  res.status(201).json(reqbody);
 };
 
 module.exports = validaCadastro;
