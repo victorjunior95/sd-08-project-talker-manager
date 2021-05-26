48daaecc5a4f0887cb5d41f309854f488ee1c901
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-25 23:16:44 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  if (talkers.length > 0) {
+    res.status(200).json(JSON.parse(talkers));
+  } else {
+    res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
