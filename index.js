c238a60d2d90f69fee8f1747f18763a981d4664c
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-05-25 23:48:03 -0300

    express-rescue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const rescue = require('express-rescue');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => {
+app.get('/talker', rescue(async (_req, res) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8')
   .then((data) => JSON.parse(data));
   if (talkers.length > 0) {
@@ -20,9 +21,9 @@ app.get('/talker', async (_req, res) => {
   } else {
     res.status(200).json([]);
   }
-});
+}));
 
-app.get('/talker/:id', async (req, res) => {
+app.get('/talker/:id', rescue(async (req, res) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8')
   .then((data) => JSON.parse(data));
   const { id } = req.params;
@@ -34,7 +35,7 @@ app.get('/talker/:id', async (req, res) => {
   } else {
     res.status(200).json(findId);
   }
-});
+}));
 
 app.listen(PORT, () => {
   console.log('Online');
