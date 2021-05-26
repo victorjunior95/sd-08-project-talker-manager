31dd9fd5ee1203b38624edff96870d34cf7d6843
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-05-26 03:30:35 -0300

    Req 2

--- routes/talkers.js
+++ routes/talkers.js
@@ -7,4 +7,14 @@ app.get('/', (_request, response) => {
   response.send(JSON.parse(fs.readFileSync('./talker.json', 'utf-8')));
 });
 
+app.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  try {
+    const talker = JSON.parse(await fs.promises.readFile('./talker.json', 'utf-8'));
+    if (talker[id - 1].name) response.send(talker[id - 1]);
+  } catch (error) {
+    response.status(404).send({ "message": "Pessoa palestrante não encontrada" });
+  }
+});
+
 module.exports = app;
