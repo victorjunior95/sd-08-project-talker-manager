84239fa8d3889e5d576126a9b41dc95ee117723d
amandaos <amanda.775.souza@gmail.com>
2021-05-26 16:21:48 -0300

    fix: corrige condicao valida talk

--- middlewares/editaTalker.js
+++ middlewares/editaTalker.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const editaTalker = (req, res) => {
   const { id } = req.params;
   const talker = JSON.parse(fs.readFileSync('./talker.json'));
-  const index = talker.indexOf(talker.find((pessoa) => pessoa.id === id));
+  const index = talker.indexOf(talker.find((person) => person.id === id));
   const newTalker = { id: parseInt(id, 10), ...req.body };
   talker.splice(index, 1, newTalker);
   fs.writeFileSync('./talker.json', JSON.stringify(talker));
