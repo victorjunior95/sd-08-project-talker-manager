d05fa56acf4931f6f0bfe8690d240ce30bd25030
Thais Quintela <thais.s.quintela@gmail.com>
2021-05-26 16:43:39 -0300

    Requisitos 1, 2 e 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const Crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +13,61 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', (_req, res) => {
+  const allTalkers = JSON.parse(fs.readFileSync('talker.json'));
+  res.status(200).json(allTalkers);
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const allTalkers = JSON.parse(fs.readFileSync('talker.json'));
+
+  const specificTalker = allTalkers.find((talker) => (String(talker.id) === id));
+  if (!specificTalker) {
+    return res.status(404).json(
+      { message: 'Pessoa palestrante não encontrada' },
+  ); 
+}
+  return res.status(200).json(specificTalker);
+});
+
+function randomString(size = 16) {  
+  return Crypto
+    .randomBytes(size)
+    .toString('base64')
+    .slice(0, size);
+}
+
+app.post('/login', [
+  (req, res, next) => {
+    if (!req.body.email || req.body.email.length === 0) {
+      return res.status(400)
+      .json({ message: 'O campo "email" é obrigatório' }); 
+  }
+  const { email } = req.body;
+    if (!/^[A-Z0-9._-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i.test(email)) {
+      return res.status(400)
+        .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+    }
+    next();
+  },
+  (req, res, next) => {
+    if (!req.body.password || req.body.password.length === 0) {
+      return res.status(400)
+        .json({ message: 'O campo "password" é obrigatório' }); 
+    }
+    if (req.body.password.length < 6) {
+      return res.status(400)
+        .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+    }
+    next();
+  },
+  (req, res) => {
+    const token = randomString();
+    res.status(200).send({ token });
+  },
+]);
 
 app.listen(PORT, () => {
   console.log('Online');
