a118cf3562247ec5d268c32cc6157133f7d2c9da
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-05-26 00:22:21 -0300

    requisito 2 - retorna o talker correspondente ao id passado na request

--- index.js
+++ index.js
@@ -10,16 +10,27 @@ const PORT = '3000';
 const rescue = require('express-rescue');
 const talkersFile = require('./middleware/fs-readFile');
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
 app.get('/talker', 
 rescue(async (_req, res, _next) => {
   const talkers = await talkersFile.getTalker();
   res.status(200).json(talkers);
 }));
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
+app.get('/talker/:id', 
+rescue(async (req, res, _next) => {
+  const talkers = await talkersFile.getTalker();
+  const idParams = parseInt(req.params.id, 10);
+  const talker = talkers.find(({ id }) => id === idParams);
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talker);
+}));
 
 app.listen(PORT, () => {
   console.log('Online');
