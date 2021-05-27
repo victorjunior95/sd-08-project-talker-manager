2d08c23951404e31f93d146ae9b0a1fe209d84b3
devFelipeFranca <felipefrancafj@gmail.com>
2021-05-27 14:30:55 -0300

    Updating...

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/talker/:id', async (_request, response) => {
   const mockData = await fs.readFile(file);
   const { id } = _request.params;
   const talker = JSON.parse(mockData).find(
-    (eachTalker) => eachTalker.id === parseInt(10, id)
+    (eachTalker) => eachTalker.id === parseInt(10, id),
   );
   if (!talker) {
     return response.status(HTTP_NOT_FOUND_STATUS).send({
