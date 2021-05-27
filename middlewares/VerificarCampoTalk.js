fa2f8c58f3f15f958ce1be3793c39332ea0dde28
Bruno Wesley <bruno.wesley77@gmail.com>
2021-05-27 15:39:45 -0300

    corrigir varificarCampoTalk

--- middlewares/VerificarCampoTalk.js
+++ middlewares/VerificarCampoTalk.js
@@ -1,11 +1,11 @@
 const verificarCampoTalk = (req, res, next) => {
   const { talk } = req.body;
-  const { watchedAt, rate } = req.body.talk;
-  
   if (!talk) {
     return res.status(400)
     .send({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
+  
+  const { watchedAt, rate } = req.body.talk;
   if (rate === undefined || watchedAt === undefined) {
     return res.status(400)
     .send({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
