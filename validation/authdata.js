bd620821b83dad02b6ececd501a19ac6fa633591
Uilton Rocha <itoclick@gmail.com>
2021-05-28 07:56:54 -0300

    req 4

--- validation/authdata.js
+++ validation/authdata.js
@@ -2,7 +2,7 @@ const regxp = /[1-3][0-9]\/[0-1][0-9]\/[0-2][0-9][0-9][0-9]/;
 const authdata = (req, res, next) => {
   const { watchedAt } = req.body.talk;
   
-  if (!regxp.test(+watchedAt)) {
+  if (!regxp.test(watchedAt)) {
     return res.status(400).json(
       { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
     ); 
