f7f0e34207089f6a7e56bd80556413a792113724
Uilton Rocha <itoclick@gmail.com>
2021-05-28 07:47:02 -0300

    req 4

--- validation/authToken.js
+++ validation/authToken.js
@@ -1,6 +1,6 @@
 const authToken = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(typeof authorization);
+  
   if (!authorization) { res.status(401).send({ message: 'Token não encontrado' }); }
   
   if (authorization.length < 16) { res.status(401).send({ message: 'Token inválido' }); }
