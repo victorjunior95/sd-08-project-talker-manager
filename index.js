b92eb4be3a6cbb79a2742653e6b560b8347fec9a
Vitor Nunes <vitornunes200@gmail.com>
2021-05-29 10:20:28 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ const PORT = '3000';
 
 const fs = require('fs');
 
-// const rawdata = fs.readFileSync('talker.json', 'utf-8');
-// const list = JSON.parse(rawdata);
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', (req, res, _next) => {
-  // fs.readFile('talker.json', (err, data) => {
-  //   const list = JSON.parse(data);
-  //   res.status(200).send(list);
-  // });
   const list = JSON.parse(fs.readFileSync('talker.json'));
   res.status(200).send(list);
+});
+
+app.get('/talker/:id', (req, res, _next) => {
+  const { id } = req.params;
+  const list = JSON.parse(fs.readFileSync('talker.json'));
+  const search = list.find((index) => index.id === parseInt(id, 10));
+  if (search === undefined) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).send(search);
 });
 
 app.listen(PORT, () => {
