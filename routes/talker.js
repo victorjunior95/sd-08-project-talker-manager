45e083fabfe1ced2f699e83a75df555893668a39
Ana Karine <anakarine.ti@gmail.com>
2021-05-29 18:27:00 -0300

    req 2

--- routes/talker.js
+++ routes/talker.js
@@ -12,4 +12,15 @@ app.get('/', (req, res) => {
   res.status(200).json(allTalkers);
 });
 
+// localhost:3000/talker/:id
+app.get('/:id', (req, res) => {
+  const { id: reqId } = req.params;
+  const allTalkers = JSON.parse(fs.readFileSync(`${__dirname}/../talker.json`));
+  const talkerById = allTalkers.find(({ id }) => id === Number(reqId));
+  if (talkerById) {
+    return res.status(200).json(talkerById);
+  }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = app;
