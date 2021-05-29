0873fcb3b11e2b7323363919fdfe5644753de291
Vitor Nunes <vitornunes200@gmail.com>
2021-05-29 09:26:02 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const fs = require('fs');
+
+const rawdata = fs.readFileSync('talker.json');
+let list = JSON.parse(rawdata);
+
+app.get('/talker', (req, res, _next) => {
+  if (list.lenght === 0 || list === null) {
+    list = [];
+  }
+  res.status(200).send(list);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
