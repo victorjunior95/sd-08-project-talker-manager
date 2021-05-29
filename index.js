68eaa3ffd3d71285bd35c6aa56afc2cf3cb7985f
Vitor Nunes <vitornunes200@gmail.com>
2021-05-29 10:02:39 -0300

    Requisito1 refatorado

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const PORT = '3000';
 
 const fs = require('fs');
 
-const rawdata = fs.readFileSync('talker.json');
-let list = JSON.parse(rawdata);
-
-app.get('/talker', (req, res, _next) => {
-  if (list.lenght === 0 || list === null) {
-    list = [];
-  }
-  res.status(200).send(list);
-});
+// const rawdata = fs.readFileSync('talker.json', 'utf-8');
+// const list = JSON.parse(rawdata);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res, _next) => {
+  // fs.readFile('talker.json', (err, data) => {
+  //   const list = JSON.parse(data);
+  //   res.status(200).send(list);
+  // });
+  const list = JSON.parse(fs.readFileSync('talker.json'));
+  res.status(200).send(list);
 });
 
 app.listen(PORT, () => {
