cd033eede508836a241521e8cb528fa6b53edb0d
albertassi88 <albertassi88@gmail.com>
2021-05-30 17:14:23 -0300

    Corrigindo erros

--- routes/createTalker.js
+++ routes/createTalker.js
@@ -2,14 +2,11 @@ const fs = require('fs');
 const rescue = require('express-rescue');
 const talk = require('../talker.json');
 
-const createTalker = rescue(async (req, res) => {
-    const { name, age, talk: { watchedAt, rate } } = req.body;
-    const newFile = { name, age, talk: { watchedAt, rate } };
+const createTalker = rescue(async (req, res) => {   
     const file = talk;
-    const id = Math.max(...talk.map((element) => element.id)) + 1;
-    const newTalker = { id, ...newFile };
-    file.push(newTalker);
-    const newResult = JSON.stringify(file, null, 2);
+    const newTalker = { id: file.length + 1, ...req.body };
+    const talkers = file.concat(newTalker);  
+    const newResult = JSON.stringify(talkers, null, 2);
     fs.writeFileSync('talker.json', newResult, 'utf-8'); 
     res.status(201).json(newTalker);
 });
