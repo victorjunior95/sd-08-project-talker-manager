94d1a43f9f693d3072ec33d6036081215065d242
lucasbgama <lucasbg@protonmail.com>
2021-05-30 19:54:50 -0300

    requisito 4

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/talker', (_req, resp) => {
 app.get('/talker/:id', (req, resp, next) => {
   const file = fs.readFileSync('./talker.json');
   const data = JSON.parse(file || '[]');
-  console.log(req.params.id);
   const talker = data.find(({ id }) => req.params.id === id.toString());
   if (talker !== undefined) resp.status(HTTP_OK_STATUS).json(talker);
   else {
@@ -34,7 +33,7 @@ const validateLogin = (email, password) => {
   if (password.length < 6) return 'O "password" deve ter pelo menos 6 caracteres';
 };
 
-// const tokens = [];
+const tokens = [];
 const tokenGenerate = (size = 16) => {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let token = '';
@@ -53,8 +52,67 @@ app.post('/login', (req, _resp, next) => {
   return next(error);
 }, (_req, resp) => {
   const token = tokenGenerate();
-  // tokens.push(token);
+  tokens.push(token);
   resp.json({ token });
+});
+
+const validateToken = (token) => {
+  if (token && token.length !== 16) return 'Token inválido';
+  if (!tokens.includes(token)) return 'Token não encontrado';
+};
+
+const checkTalkFields = (watchedAt, rate) => {
+  if (!/\d{2}\/\d{2}\/\d{4}/.test(watchedAt)) {
+    return 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+  }
+  if (!/^\d$/.test(rate) || parseInt(rate, 10) > 5 || parseInt(rate, 10) < 1) {
+    return 'O campo "rate" deve ser um inteiro de 1 à 5';
+  }
+};
+
+const validateTalk = (talk) => {
+  const { watchedAt, rate } = talk || {};
+  if (!talk || !watchedAt || !rate) {
+    return 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
+  }
+  return checkTalkFields(watchedAt, rate);
+};
+
+const validateTalker = (name, age, talk) => {
+  if (!name) return 'O campo "name" é obrigatório';
+  if (name.length < 3) return 'O "name" deve ter pelo menos 3 caracteres';
+  if (!age) return 'O campo "age" é obrigatório';
+  if (age < 18) return 'A pessoa palestrante deve ser maior de idade';
+  return validateTalk(talk);
+};
+
+const successAddTalk = (data) => {
+  const dataFs = fs.readFileSync('./talker.json');
+  const array = JSON.parse(dataFs);
+  const id = !array || array.length === 0 ? 1 : array.sort((a, b) => b.id - a.id)[0].id + 1;
+  const dataToBeAdded = { id, ...data };
+  const newData = [...array, dataToBeAdded];
+  fs.writeFileSync('./talker.json', JSON.stringify(newData));
+  return dataToBeAdded;
+};
+
+app.post('/talker', (req, resp, next) => {
+  const { name, age, talk } = req.body;
+  const { authorization: token } = req.headers;
+  const errToken = validateToken(token);
+  if (errToken) {
+    const err = new Error(errToken);
+    err.status = 401;
+    return next(err);
+  }
+  const errTalk = validateTalker(name, age, talk);
+  if (errTalk) {
+    const err = new Error(errTalk);
+    err.status = 400;
+    return next(err);
+  }
+  const data = successAddTalk({ name, age, talk });
+  resp.status(201).json(data);
 });
 
 app.use((err, _req, res, _next) => { res.status(err.status).json({ message: err.message }); });
