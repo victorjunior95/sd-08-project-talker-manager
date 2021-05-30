ab3a6070ddab97193e20742000d33a02f75ce7a5
Giovanna Betti <giovannabetti@gmail.com>
2021-05-29 23:54:15 -0300

    req 02

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+// Req 01
 app.get('/talker', async (_request, response) => {
   const data = await fs.readFile('./talker.json', 'utf8');
-  return response.status(HTTP_OK_STATUS).send(await JSON.parse(data));
+  return response.status(HTTP_OK_STATUS).json(await JSON.parse(data));
+});
+
+// Req 02
+app.get('talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const listaDePalestrantes = JSON.parse(fs.readFile('./talker.json', 'utf8'));
+  const pessoaPalestrante = listaDePalestrantes.find((pessoa) => pessoa.id === parseInt(id, 10));
+  if (!pessoaPalestrante) {
+    return response.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  response.status(HTTP_OK_STATUS).json(pessoaPalestrante);
 });
