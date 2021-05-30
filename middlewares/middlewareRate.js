de0626134cca030b9d63b17d17c1d1169456e157
patriciavsilva <silva.patricia.v@gmail.com>
2021-05-30 18:32:34 -0300

    vai requisito 4

--- middlewares/middlewareRate.js
+++ middlewares/middlewareRate.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   const { talk: { rate } } = req.body;
   const msg = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
-  if (rate === 0 || rate < 1 || rate > 5) {
+  if (rate < 1 || rate > 5) {
  return res.status(400)
     .json(msg); 
 }
