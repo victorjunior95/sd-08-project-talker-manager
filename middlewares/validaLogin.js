00df30985be538ac2b0182ab64eb10a80c78aa83
joao <joaocarlitoneto@gmail.com>
2021-05-30 01:56:24 -0300

    Requisito 3 - lint ok

--- middlewares/validaLogin.js
+++ middlewares/validaLogin.js
@@ -3,48 +3,36 @@ const crypto = require('crypto');
 
 function geradorDeToken() {
   return crypto.randomBytes(8).toString('hex');
-};
+}
 
 /* Trecho tirado stackoverflow */
-/*https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript*/
+/* https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail */
 function validaEmail(email) {
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(String(email).toLowerCase());
-};
+  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  return regex.test(email);
+}
 
 const validaLogin = (request, response) => {
   const { email, password } = request.body;
   const token = geradorDeToken();
 
-  if (!email) {
-    return response.status(400).send({ 
-      message: 'O campo "email" é obrigatório'
-    });
-  };
+  if (!email) return response.status(400).send({ message: 'O campo "email" é obrigatório' });
 
   if (!validaEmail(email)) {
-    return response.status(400).send({
-      message: 'O "email" deve ter o formato "email@email.com'
-    });
-  };
+    return response.status(400).send({ message: 'O "email" deve ter o formato "email@email.com' });
+  }
 
   if (!password) {
-    return response.status(400).send({
-      message: 'O campo "password" é obrigatório'
-    });
-  };
+    return response.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
 
   if (password.length < 6) {
-    return response.status(400).send({
-      message: 'O "password" deve ter pelo menos 6 caracteres'
-    });
-  };
+    return response.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } 
 
-  return response.status(200).send({
-    token
-  });
+  return response.status(200).send({ token });
 };
 
-module.exports = {
+module.exports = { 
   validaLogin,
-}
+};
