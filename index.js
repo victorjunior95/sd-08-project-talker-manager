a34cb895d4b23dbdd5ce17ad401bae62283a668f
Giovanna Betti <giovannabetti@gmail.com>
2021-05-29 23:30:21 -0300

    req 01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_request, response) => {
+  const data = await fs.readFile('./talker.json', 'utf8');
+  return response.status(200).json(await JSON.parse(data));
 });
