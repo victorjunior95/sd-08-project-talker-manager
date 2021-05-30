b9bbb57c37055240edb60e53700611a0c8a00d83
joao <joaocarlitoneto@gmail.com>
2021-05-30 12:15:43 -0300

    Requisito 3 - testes ok - lint ok

--- middlewares/validaLogin.js
+++ middlewares/validaLogin.js
@@ -1,38 +1,20 @@
-/* Trecho de código tirado do Course, Exercícios-Bônus-26.4 */
 const crypto = require('crypto');
 
-function geradorDeToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
-
-/* Trecho tirado stackoverflow */
-/* https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail */
-function validaEmail(email) {
-  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-  return regex.test(email);
-}
-
-const validaLogin = (request, response) => {
+module.exports = (request, response) => {
+  /* Trecho de código tirado do Course, Exercícios-Bônus-26.4 */
+  const token = crypto.randomBytes(8).toString('hex');
   const { email, password } = request.body;
-  const token = geradorDeToken();
-
+  const regexFormatoEmailValido = /[a-z]\w+@\w+.\w+/.test(email);
+  
   if (!email) return response.status(400).send({ message: 'O campo "email" é obrigatório' });
-
-  if (!validaEmail(email)) {
-    return response.status(400).send({ message: 'O "email" deve ter o formato "email@email.com' });
+  if (!regexFormatoEmailValido) {
+     return response.status(400).send({ 
+       message: 'O "email" deve ter o formato "email@email.com"',
+      });
   }
-
-  if (!password) {
-    return response.status(400).send({ message: 'O campo "password" é obrigatório' });
-  }
-
+  if (!password) return response.status(400).send({ message: 'O campo "password" é obrigatório' });
   if (password.length < 6) {
     return response.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  } 
-
+  }
   return response.status(200).send({ token });
-};
-
-module.exports = { 
-  validaLogin,
 };
