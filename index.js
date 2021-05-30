3ca9dd5c4a6a8d430d8afc8c14bd3d35da77bf91
albertassi88 <albertassi88@gmail.com>
2021-05-30 16:56:09 -0300

    Corrigindo erros

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const middlewares = require('./middlewares');
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 app.use(bodyParser.json());
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
