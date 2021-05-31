70e9f3fbe5d017e222c21cf70b0d29e86942d3d4
Mario Pardo <mariio.pardojr@gmail.com>
2021-05-31 11:11:22 -0300

    Retry

--- middlewares/validateDateAndRate.js
+++ middlewares/validateDateAndRate.js
@@ -3,10 +3,10 @@ module.exports = (req, res, next) => {
   const regexDate = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
   const dateIsValid = regexDate.test(watchedAt);
   if (rate < 1 || rate > 5) {
-    res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
   if (!dateIsValid) {
-    res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
   // if (!rate) {
   //   res.status(400)
--- middlewares/validateTalk.js
+++ middlewares/validateTalk.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
   if (req.body.talk === undefined) {
-    res.status(400)
+    return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
   if (req.body.talk.watchedAt === undefined || req.body.talk.rate === undefined) {
-    res.status(400)
+    return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
   next();
