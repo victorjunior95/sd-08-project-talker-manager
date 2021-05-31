b89681a4917d606897047b5814d9a8e51ebbdfaf
Raul Afonso <raulafonso@Rauls-Mac-mini.local>
2021-05-31 10:13:55 -0300

    requisito 2 e 3

--- index.js
+++ index.js
@@ -7,18 +7,48 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const rescue = require('express-rescue');
+const crypto = require('crypto');
+const talkersFile = require('./middleware/fs-readFile');
+
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
-const rescue = require('express-rescue');
-const talkersFile = require('./middlewares/readFile');
 
 app.get('/talker', 
 rescue(async (_req, res, _next) => {
   const talkers = await talkersFile.getTalker();
   res.status(200).json(talkers);
 }));
+
+app.get('/talker/:id', 
+rescue(async (req, res, _next) => {
+  const talkers = await talkersFile.getTalker();
+  const idParams = parseInt(req.params.id, 10);
+  const talker = talkers.find(({ id }) => id === idParams);
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talker);
+}));
+
+app.post('/login', (req, res, _next) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!email.match(/(.+@.+\.com)(\.br)?/)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
