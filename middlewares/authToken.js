90a889129b94f221603e7399b3cd866c01f63379
Mario Pardo <mariio.pardojr@gmail.com>
2021-05-31 11:16:31 -0300

    Adiciona return nos middlewares

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    res.status(401).json({ message: 'Token não encontrado' });
+    return res.status(401).json({ message: 'Token não encontrado' });
   }
   if (authorization.length !== 16) {
-    res.status(401).json({ message: 'Token inválido' });
+    return res.status(401).json({ message: 'Token inválido' });
   }
   next();
 };
