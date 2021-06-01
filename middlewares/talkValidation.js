3538f71a59012ffb0b5dcba7ae57d7e8d304ba0e
caiomzrocha <caiomzrocha@gmail.com>
2021-06-01 17:44:25 -0300

    [REQ-04][FIX] Reduz a quantidade de linhas da arrow function talkValidation.

--- middlewares/talkValidation.js
+++ middlewares/talkValidation.js
@@ -7,17 +7,14 @@ const talkValidation = (req, res, next) => {
       message: 'O camp "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
   }
-
   if (!talk.watchedAt.match(dateRegex)) {
     return res.status(400).json({
       message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
   }
-
   if (!(Number.isInteger(talk.rate) && talk.rate >= 1 && talk.rate <= 5)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
-
   return next();
 };
 
