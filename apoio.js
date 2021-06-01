dd868a8d6407c90f06675c412ef0c0bcdf34cede
Leonardo <lecastro16@gmail.com>
2021-06-01 18:42:01 -0300

    requisito 1 v1

--- apoio.js
+++ apoio.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 
-function readFilePromise (fileName) {
+function readFilePromise(fileName) {
   return new Promise((resolve, reject) => {
-
     fs.readFile(fileName, (err, content) => {
       if (err) return reject(err);
       resolve(content);
     });
-
   });
 }
 
-
 readFilePromise('./talker.json') // A função me promete que vai ler o arquivo
-  .then((content) => { // Caso ela cumpra o que prometeu
+  .then((content) => {
+    // Caso ela cumpra o que prometeu
     console.log(`Lido arquivo com ${content} bytes`); // Escrevo o resultado no console
   })
-  .catch((err) => { // Caso ela não cumpra o que prometeu
+  .catch((err) => {
+    // Caso ela não cumpra o que prometeu
     console.error(`Erro ao ler arquivo: ${err.message}`); // Escrevo o erro no console
   });
