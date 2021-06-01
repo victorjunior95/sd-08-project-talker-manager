74d88aaedeecdd316dbe46820023a5bd4ed405d2
caiomzrocha <caiomzrocha@gmail.com>
2021-06-01 19:42:48 -0300

    [REQ-05][FIX] Adiciona o radix parameter na função parseInt().

--- middlewares/putTalker.js
+++ middlewares/putTalker.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const putTalker = (req, res, _next) => {
   const { id } = req.params;
   const editedTalker = req.body;
-  editedTalker.id = parseInt(id);
+  editedTalker.id = parseInt(id, 10);
 
   const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
-  const talkerIndex = talkers.findIndex((talker) => talker.id === parseInt(id));
+  const talkerIndex = talkers.findIndex((talker) => talker.id === parseInt(id, 10));
 
   talkers[talkerIndex] = editedTalker;
   fs.writeFileSync('./talker.json', JSON.stringify(talkers));
