d47694d67bd246f6e95cd0ac07e64b7565c1ed05
Leonardo <lecastro16@gmail.com>
2021-06-01 18:38:33 -0300

    requisito 1 v0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Leonardo Sardinha
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,9 +9,29 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function readFilePromise (fileName) {
+  return new Promise((resolve, reject) => {
+
+    fs.readFile(fileName, (err, content) => {
+      if (err) return reject(err);
+      resolve(content);
+    });
+
+  });
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/teste', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send('Teste do aplicativo');
+});
+
+app.get('/talker', async (_request, response) => {
+  const retorno = await readFilePromise('./talker.json')
+  response.status(HTTP_OK_STATUS).send(retorno);
 });
 
 app.listen(PORT, () => {
