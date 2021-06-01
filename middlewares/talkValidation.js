144932d7db043cef4e75ee60bcb325b81b23cb46
caiomzrocha <caiomzrocha@gmail.com>
2021-06-01 18:40:45 -0300

    [REQ-04][FIX] Correção ortográfica.

--- middlewares/talkValidation.js
+++ middlewares/talkValidation.js
@@ -3,7 +3,7 @@ const talkValidation = (req, res, next) => {
 
   if (!talk || !talk.watchedAt || !talk.rate) {
     return res.status(400).json({
-      message: 'O camp "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
   }
 
