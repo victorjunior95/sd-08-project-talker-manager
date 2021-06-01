58be25a8391018b628d92b8434044a642adbe645
Blucilha <blucilha.nascimento@gmail.com>
2021-06-01 18:48:12 -0300

    desafios 5 e 6 com o index

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const desafio01 = require('./desafios/desafio01');
 const desafio02 = require('./desafios/router02');
 const desafio03 = require('./desafios/router03');
 const desafio04 = require('./desafios/router04');
+const desafio05 = require('./desafios/router05');
+const desafio06 = require('./desafios/router06');
 
 const app = express();
-app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+app.use(bodyParser.json());
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -22,15 +24,15 @@ app.get('/talker',
 
     res
       .status(HTTP_OK_STATUS)
-      .send(readTalkers);
+      .json(readTalkers);
   }),
   (_err, _req, res, _next) => {
     res
     .status(HTTP_OK_STATUS)
-    .send([]);
+    .json([]);
   });
 
-  app.use('/talker', desafio02, desafio04);
+  app.use('/talker', desafio02, desafio04, desafio05, desafio06);
   app.use('/login', desafio03);
 
 app.listen(PORT, () => {
