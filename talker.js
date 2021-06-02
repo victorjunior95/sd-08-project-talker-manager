220fe50dd9e4048b1efb6278496580f2d699e29e
Bianca <bruninhakjurugat11@gmail.com>
2021-06-02 17:46:43 -0300

    req 2

--- talker.js
+++ talker.js
@@ -11,4 +11,19 @@ router.get('/', rescue(async (_, res) => {
     res.status(200).json(talkers);
 }));
 
+router.get('/:id', rescue(async (req, res) => {
+    const { id } = req.params;
+
+    const talkers = await fs.readFile('./talker.json')
+        .then((data) => JSON.parse(data));
+
+    const talker = talkers.find((t) => parseInt(t.id, 10) === parseInt(id, 10));
+
+    if (!talker) {
+        res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    res.status(200).json(talker);
+}));
+
 module.exports = router;
