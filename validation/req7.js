113ef06e8a56a1ee286e14fda621510a09b76445
Uilton Rocha <itoclick@gmail.com>
2021-06-03 09:15:38 -0300

    req-07

--- validation/req7.js
+++ validation/req7.js
@@ -1,8 +1,11 @@
 const talkers = require('../helpers/talkers');
 
 const req7 = (req, res) => {
-  const retornoPesquisa = talkers().filter((tk) => tk.name.includs(req.query.q));
-res.status(201).json(retornoPesquisa);
+  console.log('q tem:', req.query.q, 'talkers tem:', talkers());
+  const retornoPesquisa = talkers().filter((tk) =>
+   tk.name[0] === req.query.q);
+
+res.status(200).json(retornoPesquisa);
 };
 
 module.exports = req7; 
