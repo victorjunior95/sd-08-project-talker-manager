205f4ac6076983917e44c6d904c6bb45728b3664
Fabio <fabiohigoralm2@gmail.com>
2021-06-02 22:53:18 -0300

    Req 7

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// 7 - Crie o endpoint GET /talker/search?q=searchTerm
+app.get('/talker/search', middlewares.validationToken, async (req, res) => {
+  const file = await fsfunctions.readDataTalkers();
+  const { q } = req.query;
+  if (q) {
+    const verifyName = file.filter(({ name }) => name.includes(q));
+     res.status(200).json(verifyName);
+  }
+   res.status(200).json(file);
+});
+
 // 1 - Crie o endpoint GET /talker 
 app.get('/talker', async (req, res) => {
   const file = await fsfunctions.readDataTalkers();
@@ -35,39 +47,40 @@ app.get('/talker/:id', async (req, res) => {
 app.post('/login', middlewares.login);
 
 // 4 - Crie o endpoint POST /talker
-app.post('/talker', middlewares.validationToken, middlewares.validationName, 
-middlewares.validationAge, middlewares.validationTalker, middlewares.validationDate,
-middlewares.validationRate, async (req, res) => {
-  const file = await fsfunctions.readDataTalkers();
-  const registreTalker = req.body;
-  registreTalker.id = file.length + 1;
-  file.push(registreTalker);
-  await fsfunctions.writeDataTalkers(file);
-  res.status(201).json(registreTalker);
-});
-// 5 - Crie o endpoint PUT /talker/:id
-app.put('/talker/:id', middlewares.validationToken, middlewares.validationName, 
-middlewares.validationAge, middlewares.validationTalker, middlewares.validationDate,
-middlewares.validationRate, rescue(async (req, res) => {
-  const id = Number(req.params.id);
-  const file = await fsfunctions.readDataTalkers();
-  const editTalker = { ...req.body, id };
-  const verifyTalker = file.map((data) => {
-    if (data.id === id) return editTalker;
-    return data;
-  });
-  await fsfunctions.writeDataTalkers(verifyTalker);
-  res.status(200).json(editTalker);
-  }));
-  
- // 6 - Crie o endpoint DELETE /talker/:id
- app.delete('/talker/:id', middlewares.validationToken, rescue(async (req, res) => {
-  const id = Number(req.params.id);
+app.post('/talker', middlewares.validationToken, middlewares.validationName,
+  middlewares.validationAge, middlewares.validationTalker, middlewares.validationDate,
+  middlewares.validationRate, async (req, res) => {
     const file = await fsfunctions.readDataTalkers();
-    const veridyDeleteById = file.find((data) => data.id !== id);
-    await fsfunctions.writeDataTalkers(veridyDeleteById);
-    res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
- })); 
+    const registreTalker = req.body;
+    registreTalker.id = file.length + 1;
+    file.push(registreTalker);
+    await fsfunctions.writeDataTalkers(file);
+    res.status(201).json(registreTalker);
+  });
+// 5 - Crie o endpoint PUT /talker/:id
+app.put('/talker/:id', middlewares.validationToken, middlewares.validationName,
+  middlewares.validationAge, middlewares.validationTalker, middlewares.validationDate,
+  middlewares.validationRate, rescue(async (req, res) => {
+    const id = Number(req.params.id);
+    const file = await fsfunctions.readDataTalkers();
+    const editTalker = { ...req.body, id };
+    const verifyTalker = file.map((data) => {
+      if (data.id === id) return editTalker;
+      return data;
+    });
+    await fsfunctions.writeDataTalkers(verifyTalker);
+    res.status(200).json(editTalker);
+  }));
+
+// 6 - Crie o endpoint DELETE /talker/:id
+app.delete('/talker/:id', middlewares.validationToken, rescue(async (req, res) => {
+  const id = Number(req.params.id);
+  const file = await fsfunctions.readDataTalkers();
+  const veridyDeleteById = file.find((data) => data.id !== id);
+  await fsfunctions.writeDataTalkers(veridyDeleteById);
+  res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+}));
+
 app.listen(PORT, () => {
   console.log('Online');
 });
