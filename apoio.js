6d0d8b2c3b54053f5e3fb5ce95ad02d2be1b094f
Leonardo <lecastro16@gmail.com>
2021-06-04 09:55:54 -0300

    requisito 2 v0

--- apoio.js
+++ apoio.js
@@ -18,3 +18,12 @@ readFilePromise('./talker.json') // A função me promete que vai ler o arquivo
     // Caso ela não cumpra o que prometeu
     console.error(`Erro ao ler arquivo: ${err.message}`); // Escrevo o erro no console
   });
+
+  const getAllTalkers = () => JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+
+const getTalkerById = (id) => {
+  const allTalkers = getAllTalkers();
+  const talker = allTalkers.filter((elem)=> elem.id == id )
+  return talker;
+}
+
--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const PORT = '3000';
 
 const getAllTalkers = () => JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
 
+const getTalkerById = (id) => {
+  const allTalkers = getAllTalkers();
+  const talker = allTalkers.filter((elem)=> elem.id == id );
+  console.log(talker[0]);
+  return talker[0];
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -29,6 +36,21 @@ app.get('/talker', async (_request, response) => {
   }
 });
 
+app.get('/talker/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    idParse = parseInt(id, 10);
+    const talkerFound = await getTalkerById(idParse);
+    response.status(HTTP_OK_STATUS).send(talkerFound);
+  } catch (err) {
+    response.status(404).send({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+getTalkerById(1);
