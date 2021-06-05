8652921053b83ef14af35da83589e1183d92d0c3
UmberBn <humberto-bandeira@hotmail.com>
2021-06-05 16:03:11 -0400

    adicionando referencia de token aleatorio

--- Routes/login.js
+++ Routes/login.js
@@ -4,6 +4,7 @@ const passwordMiddleware = require('../middlewares/validation/password');
 const app = require('../utils/express');
 
 app.post('/', emailMiddleware, passwordMiddleware, (req, res) => {
+  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
   const token = ((crypto.randomBytes(20).toString('hex'))).substring(0, 16);
   res.status(200);
   res.json({ token });
