05036678dd4e68b33c792476d1e7f2e322b4d115
José Bernardo Johann <josebernardojohann@gmail.com>
2021-06-08 12:54:51 -0300

    Little change on my index.js file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -12,6 +11,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port 3000.');
