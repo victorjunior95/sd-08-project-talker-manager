21a95ee82e68828aeb8fd07ccf3da2f12e4a2651
Leonardo <lecastro16@gmail.com>
2021-06-09 19:28:19 -0300

    requisito 4 v3

--- postMiddleware.js
+++ postMiddleware.js
@@ -1,12 +1,10 @@
 function validToken(req, res, next) {
   const { token } = req.headers;
   console.log('header', token);
-  if (!token)
-    return res.status(401).send({
+  if (!token) return res.status(401).send({
       message: "Token não encontrado",
     });
-  if (token.length != 16)
-    res.status(400).send({
+  if (token.length != 16) return res.status(400).send({
       message: "Token inválido",
     });
   next();
@@ -17,16 +15,13 @@ function validNameAndAge(req, res, next) {
   console.log("new talker", newTalker);
   const { name, age } = newTalker;
 
-  if (!name)
-    return res.status(400).send({
+  if (!name) return res.status(400).send({
       message: 'O campo "name" é obrigatório',
     });
-  if (name.length < 3)
-    return res.status(400).send({
+  if (name.length < 3) return res.status(400).send({
       message: 'O "name" deve ter pelo menos 3 caracteres',
     });
-  if (!age)
-    return res.status(400).send({
+  if (!age) return res.status(400).send({
       message: 'O campo "age" é obrigatório',
     });
   next();
@@ -39,12 +34,10 @@ function validTalker(req, res, next) {
   } = newTalker;
   const dateFormat =
     /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
-  if (!dateFormat.test(watchedAt))
-    return res.status(400).send({
+  if (!dateFormat.test(watchedAt)) return res.status(400).send({
       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
     });
-  if (!Number.isInteger(rate) || rate < 1 || rate > 5)
-    return res.status(400).send({
+  if (!Number.isInteger(rate) || rate < 1 || rate > 5) return res.status(400).send({
       message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
