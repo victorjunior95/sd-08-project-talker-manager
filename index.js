b798f5a276bb85213331f88f8bc66f170dac0b54
Breenolf <blf@ic.ufal.br>
2021-06-09 22:08:21 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const getTalkers = () => fs.readFileSync('./talker.json', 'utf-8');
+
+// console.log(talkerFile);
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +15,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  try {
+    const talkerFile = getTalkers();
+    return res.status(HTTP_OK_STATUS).send(JSON.parse(talkerFile));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 app.listen(PORT, () => {
