f5bc48ed39bff06e905e069b1052879d487d498c
Leonardo <lecastro16@gmail.com>
2021-06-10 13:28:25 -0300

    requisito 4 v10

--- authMiddleware.js
+++ authMiddleware.js
@@ -21,7 +21,7 @@ function authPassword(req, res, next) {
       message: 'O campo "password" é obrigatório',
     });
   }
-  if (password < 6) {
+  if (password.length < 6) {
     return res.status(400).json({
       message: 'O "password" deve ter pelo menos 6 caracteres',
     });
