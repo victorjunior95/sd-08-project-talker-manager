16969c274e70fe3b098621e187d722f855c23009
wagnerberna <cabelo29007@gmail.com>
2021-06-13 09:29:29 -0300

    Req03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const path = './talker.json';
 
@@ -33,6 +34,31 @@ app.get('/talker/:id', async (req, resp) => {
     return resp.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   return resp.status(200).json(searchId);
+});
+
+// Req03
+function loginCheck(req, resp, next) {
+  const { email, password } = req.body;
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  const emailCheckFormat = emailRegex.test(email);
+  if (!email) {
+    return resp.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailCheckFormat) {
+    return resp.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return resp.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.toString().length < 6) {
+    return resp.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return next();
+}
+
+app.post('/login', loginCheck, async (req, resp) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return resp.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
