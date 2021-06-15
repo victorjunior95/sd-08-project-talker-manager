9ec4f39d505e0779595d109d15ea41e6a2dc0733
victorjunior95 <victorjr.muchacho@gmail.com>
2021-06-15 20:22:31 -0300

    Challenge 1 completed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const database = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  fs.readFile(database, (err, data) => {
+    res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf-8')));
+  });
 });
 
 app.listen(PORT, () => {
