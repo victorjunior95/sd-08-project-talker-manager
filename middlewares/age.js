5fa868bf5a2f569fe717e22207a08e66d58fdc5c
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-16 18:15:54 -0300

    req 5 fix

--- middlewares/age.js
+++ middlewares/age.js
@@ -1,10 +1,10 @@
-module.exports = (request, response, next) => {
-  const { age } = request.body;
+module.exports = (req, res, next) => {
+  const { age } = req.body;
   if (!age) {
-    return response.status(400).json({ message: 'O campo "age" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   }
   if (age < 18) {
-    return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
   next();
 };
--- middlewares/rate.js
+++ middlewares/rate.js
@@ -1,7 +1,11 @@
-module.exports = (request, response, next) => {
-  const { rate } = request.body.talk;
+module.exports = (req, res, next) => {
+  const { rate } = req.body.talk;
   if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
-    return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  if (!rate) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
   next();
 }; 
