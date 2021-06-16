34d2e89a7412292ea3e015d03d27728ea0ac333c
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-16 18:31:37 -0300

    req 5 fix again

--- middlewares/rate.js
+++ middlewares/rate.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
   const { rate } = req.body.talk;
-  if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
+  if (Number(rate) < 1 || Number(rate) > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
+  
   if (!rate) {
     return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
