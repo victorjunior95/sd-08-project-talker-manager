1a454d2d451a8c34be26485a60da11481da3765a
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-16 13:57:48 -0300

    req 4 falhou tudoo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const talker = require('./service');
 const middlewares = require('./middlewares');
-const newTalker = require('./write');
+// const newTalker = require('./write');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,27 +20,27 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).json(data);
 });
 
-// app.get('/talker/:id', async (req, res) => {
-//   const data = await talker();
-//   const dataById = data.find((item) => parseInt(item.id, 0) === parseInt(req.params.id, 0));
-//   if (!dataById) {
-//     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-//   }
-//   return res.status(200).json(dataById);
-// });
+app.get('/talker/:id', async (req, res) => {
+  const data = await talker();
+  const dataById = data.find((item) => parseInt(item.id, 0) === parseInt(req.params.id, 0));
+  if (!dataById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(dataById);
+});
 
 app.post('/login', middlewares.login);
 
-app.post('/talker', middlewares.token, middlewares.name,
- middlewares.age, middlewares.talk, middlewares.watchedAt,
-  middlewares.rate, async (req, res) => {
-    const talkers = await await talker();
-    const otherTalker = req.body;
-    otherTalker.id = talkers.length + 1;
-    talkers.push(otherTalker);
-    await newTalker(talkers);
-    res.status(201).json(otherTalker);
-  });
+// app.post('/talker', middlewares.token, middlewares.name,
+//  middlewares.age, middlewares.talk, middlewares.watchedAt,
+//   middlewares.rate, async (req, res) => {
+//     const talkers = await await talker();
+//     const otherTalker = req.body;
+//     otherTalker.id = talkers.length + 1;
+//     talkers.push(otherTalker);
+//     await newTalker(talkers);
+//     res.status(201).json(otherTalker);
+//   });
 
 app.listen(PORT, () => {
   console.log('Online');
