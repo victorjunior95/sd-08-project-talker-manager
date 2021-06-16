dcc6540d7ce34351a7333cbabfb409af9807bada
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-06-16 19:51:12 -0300

    Exercicio 1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKER_PATH = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,7 +15,10 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  const allTalkers = fs.readFileSync(TALKER_PATH, 'utf8');
+  const parse = allTalkers ? JSON.parse(allTalkers) : null;
+  if (!allTalkers || parse.length === 0) return response.status(200).send({});
+  return response.status(HTTP_OK_STATUS).send(allTalkers);
 });
 
 app.listen(PORT, () => {
