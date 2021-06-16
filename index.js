4ebeebe94cebd94429eadb4b62a276323355fea1
victorjunior95 <victorjr.muchacho@gmail.com>
2021-06-16 15:58:02 -0300

    Challenge 2 and first correction

--- index.js
+++ index.js
@@ -19,20 +19,19 @@ app.get('/', (_request, response) => {
 // 1 - Crie o endpoint GET `/talker`, que deve retornar um array com todas as pessoas palestrantes cadastradas. Devendo retornar o `status 200`.
 app.get('/talker', (req, res) => {
   fs.readFile(database, (err, data) => {
-    res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf-8')));
+    res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf8')));
   });
 });
 
 app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
   fs.readFile(database, (err, data) => {
-    const { id } = req.params;
     const talkers = JSON.parse(data.toString('utf-8'));
-    const talkerFind = talkers.find((talker) => talker.id === id);
+    const talkerFind = talkers.find((talker) => talker.id === parseInt(id, 10));
     if (talkerFind) {
       return res.status(HTTP_OK_STATUS).json(talkerFind);
     }
     res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
-    // res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf-8')));
   });
 });
 
