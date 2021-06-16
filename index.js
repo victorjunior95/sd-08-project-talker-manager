93b44b0846f733025a97a8aa0e9e20acd21db8b0
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-16 18:05:21 -0300

    req 5

--- index.js
+++ index.js
@@ -42,6 +42,20 @@ app.post('/talker', middlewares.token, middlewares.name,
     res.status(201).json(otherTalker);
   });
 
+  app.put('/talker/:id', middlewares.token, middlewares.name,
+ middlewares.age, middlewares.talk, middlewares.watchedAt,
+  middlewares.rate, async (req, res) => {
+    const { id } = req.params;
+    const talkers = await talker();
+    const otherTalker = { ...req.body, id };
+    const edited = talkers.map((item) => {
+      if (item.id === id) return newTalker;
+      return item;
+    });
+    await newTalker(edited);
+    res.status(201).json(otherTalker);
+  });
+
 app.listen(PORT, () => {
   console.log('Online');
 });
