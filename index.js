f542d4660b7136111cefb3c25f4654f98d7c276c
victorjunior95 <victorjr.muchacho@gmail.com>
2021-06-16 10:49:33 -0300

    Challenge 2 completed

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
+
 const PORT = '3000';
 const database = './talker.json';
 
@@ -14,9 +16,23 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// 1 - Crie o endpoint GET `/talker`, que deve retornar um array com todas as pessoas palestrantes cadastradas. Devendo retornar o `status 200`.
 app.get('/talker', (req, res) => {
   fs.readFile(database, (err, data) => {
     res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf-8')));
+  });
+});
+
+app.get('/talker/:id', (req, res) => {
+  fs.readFile(database, (err, data) => {
+    const { id } = req.params;
+    const talkers = JSON.parse(data.toString('utf-8'));
+    const talkerFind = talkers.find((talker) => talker.id === id);
+    if (talkerFind) {
+      return res.status(HTTP_OK_STATUS).json(talkerFind);
+    }
+    res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    // res.status(HTTP_OK_STATUS).json(JSON.parse(data.toString('utf-8')));
   });
 });
 
