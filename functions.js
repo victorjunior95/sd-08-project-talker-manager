47bdcd5936964fbb145e8789197a9414f2ef83f4
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-06-16 20:45:04 -0300

    Requisito 3

--- functions.js
+++ functions.js
@@ -1,5 +1,21 @@
+const randtoken = require('rand-token');
+const validator = require('email-validator');
+
 const findTalkerByID = (talkers, id) => talkers.find((e) => e.id === Number(id));
+
+const generateToken = () => randtoken.generate(16);
+
+const verifyEmailAndPassword = (email, password, MESSAGES) => {
+  if (!email) return MESSAGES.emptyEmail;
+  if (!password) return MESSAGES.emptyPassword;
+  if (password.toString().length < 6) return MESSAGES.passwordLowerThenSix;
+  const validEmail = validator.validate(email);
+  if (!validEmail) return MESSAGES.wrongEmailFormat;
+  return false;
+};
 
 module.exports = {
   findTalkerByID,
+  generateToken,
+  verifyEmailAndPassword,
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { findTalkerByID } = require('./functions');
+const { findTalkerByID, generateToken, verifyEmailAndPassword } = require('./functions');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +11,10 @@ const PORT = '3000';
 const TALKER_PATH = './talker.json';
 const MESSAGES = {
   idNotFound: 'Pessoa palestrante não encontrada',
+  emptyEmail: 'O campo "email" é obrigatório',
+  wrongEmailFormat: 'O "email" deve ter o formato "email@email.com"',
+  emptyPassword: 'O campo "password" é obrigatório',
+  passwordLowerThenSix: 'O "password" deve ter pelo menos 6 caracteres',
 };
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -31,6 +35,14 @@ app.get('/talker/:id', (req, res) => {
   const talker = findTalkerByID(talkers, req.params.id);
   if (!talker) return res.status(404).send({ message: MESSAGES.idNotFound });
   return res.status(HTTP_OK_STATUS).send(talker);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const failedToverify = verifyEmailAndPassword(email, password, MESSAGES);
+  if (failedToverify) return res.status(400).send({ message: failedToverify });
+  const token = generateToken();
+  res.status(200).send({ token });
 });
 
 app.listen(PORT, () => {
