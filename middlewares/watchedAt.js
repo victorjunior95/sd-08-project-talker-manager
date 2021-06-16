037a15076908525d85a89324cab487b68a31632b
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-16 18:24:25 -0300

    req 5 fix again

--- middlewares/watchedAt.js
+++ middlewares/watchedAt.js
@@ -1,6 +1,10 @@
 module.exports = (req, res, next) => {
   const { talk: { watchedAt } } = req.body;
   const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!watchedAt) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
   if (!(regex.test(watchedAt))) {
    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
