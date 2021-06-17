736535ae7bb4fbe68e6c19398ffbda6eb0800953
Isabella Paz <bellapaz.iorp@gmail.com>
2021-06-17 04:27:16 -0300

    index.js

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const { login, talker } = require('./Routes');
+const { errorHandler } = require('./Helpers');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/login', login);
+app.use('/talker', talker);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Online');
