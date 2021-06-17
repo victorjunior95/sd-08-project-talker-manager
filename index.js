9548044cb4cc11bbd41ed4ec9e491b6cc4ea1787
Andre Sampaio <andresampaiolm@gmail.com>
2021-06-17 04:13:51 -0300

    REQ 01

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (req, res) => {
+  const data = fs.readFileSync('talker.json', 'utf8');
+
+  if (data) {
+    res.status(HTTP_OK_STATUS).send(data);
+  } else {
+    res.status(HTTP_OK_STATUS).send([]);
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
