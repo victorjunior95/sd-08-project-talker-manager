276d8f145cec8b506aa1c20a762ef823c250be30
victorjunior95 <victorjr.muchacho@gmail.com>
2021-06-17 01:14:33 -0300

    Challenge 4 and third correction

--- middlewares/name.js
+++ middlewares/name.js
@@ -6,7 +6,7 @@ const errNoName = 'O campo "name" é obrigatório';
 const errNameInvalid = 'O "name" deve ter pelo menos 3 caracteres';
 
 module.exports = (req, res, next) => {
-  const { name } = req.headers;
+  const { name } = req.body;
   if (!name) return res.status(BAD_REQUEST_STATUS).json({ message: errNoName });
 
   const isNameValidate = utils.validName(name);
